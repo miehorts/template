f98cec320f8ad2415c892487f04a647a1c2a4f2a
miehorts <stroheimwt@yahoo.co.jp>
2023-08-05 19:47:50 +0900

    commit

--- main.js
+++ main.js
@@ -2,5 +2,5 @@ const information = document.querySelector('div.information');
 information.innerHTML = window.innerHeight;
 const mainH = document.querySelector('div.main-back').offsetHeight;
 const buttonH = document.querySelector('div.button-area').offsetHeight;
-information.getElementsByClassName.height
+information.style.height
 = (window.innerHeight - mainH - buttonH) + 'px';
