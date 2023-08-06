0a94623157a545701243ac94bb47d059bfd034f5
miehorts <stroheimwt@yahoo.co.jp>
2023-08-06 17:48:10 +0900

    commit

---
+++ JreYokohama.js
@@ -0,0 +1,147 @@
+const JreYokohamaCommon = {
+    compName: '東日本旅客鉄道',
+    lineName: '横浜線',
+}
+
+const JreYokohama = [
+    {
+        ekimeiKanj: '東神奈川',
+        ekimeiKana: 'ひがしかながわ',
+        ekimeiRoma: 'Higashi-Kanagawa',
+        sttnAddr: '神奈川県横浜市神奈川区東神奈川一丁目',
+        ...JreYokohamaCommon
+    },
+    {
+        ekimeiKanj: '大口',
+        ekimeiKana: 'おおぐち',
+        ekimeiRoma: 'Ōguchi',
+        sttnAddr: '神奈川県横浜市神奈川区大口通',
+        ...JreYokohamaCommon
+    },
+    {
+        ekimeiKanj: '菊名',
+        ekimeiKana: 'きくな',
+        ekimeiRoma: 'Kikuna',
+        sttnAddr: '神奈川県横浜市港北区菊名七丁目',
+        ...JreYokohamaCommon
+    },
+    {
+        ekimeiKanj: '新横浜',
+        ekimeiKana: 'しんよこはま',
+        ekimeiRoma: 'Shin-Yokohama',
+        sttnAddr: '神奈川県横浜市港北区篠原町',
+        ...JreYokohamaCommon
+    },
+    {
+        ekimeiKanj: '小机',
+        ekimeiKana: 'こづくえ',
+        ekimeiRoma: 'Kozukue',
+        sttnAddr: '神奈川県横浜市港北区小机町',
+        ...JreYokohamaCommon
+    },
+    {
+        ekimeiKanj: '鴨居',
+        ekimeiKana: 'かもい',
+        ekimeiRoma: 'Kamoi',
+        sttnAddr: '神奈川県横浜市緑区鴨居一丁目',
+        ...JreYokohamaCommon
+    },
+    {
+        ekimeiKanj: '中山',
+        ekimeiKana: 'なかやま',
+        ekimeiRoma: 'Nakayama',
+        sttnAddr: '神奈川県横浜市緑区寺山町',
+        ...JreYokohamaCommon
+    },
+    {
+        ekimeiKanj: '十日市場',
+        ekimeiKana: 'とおかいちば',
+        ekimeiRoma: 'Tōkaichiba',
+        sttnAddr: '神奈川県横浜市緑区十日市場町',
+        ...JreYokohamaCommon
+    },
+    {
+        ekimeiKanj: '長津田',
+        ekimeiKana: 'ながつた',
+        ekimeiRoma: 'Nagatsuta',
+        sttnAddr: '神奈川県横浜市緑区長津田四丁目',
+        ...JreYokohamaCommon
+    },
+    {
+        ekimeiKanj: '成瀬',
+        ekimeiKana: 'なるせ',
+        ekimeiRoma: 'Naruse',
+        sttnAddr: '東京都町田市南成瀬一丁目',
+        ...JreYokohamaCommon
+    },
+    {
+        ekimeiKanj: '町田',
+        ekimeiKana: 'まちだ',
+        ekimeiRoma: 'Machida',
+        sttnAddr: '東京都町田市原町田',
+        ...JreYokohamaCommon
+    },
+    {
+        ekimeiKanj: '古淵',
+        ekimeiKana: 'こぶち',
+        ekimeiRoma: 'Kobuchi',
+        sttnAddr: '神奈川県相模原市南区古淵二丁目',
+        ...JreYokohamaCommon
+    },
+    {
+        ekimeiKanj: '淵野辺',
+        ekimeiKana: 'ふちのべ',
+        ekimeiRoma: 'Fuchinobe',
+        sttnAddr: '神奈川県相模原市中央区淵野辺三丁目',
+        ...JreYokohamaCommon
+    },
+    {
+        ekimeiKanj: '矢部',
+        ekimeiKana: 'やべ',
+        ekimeiRoma: 'Yabe',
+        sttnAddr: '神奈川県相模原市中央区矢部三丁目',
+        ...JreYokohamaCommon
+    },
+    {
+        ekimeiKanj: '相模原',
+        ekimeiKana: 'さがみはら',
+        ekimeiRoma: 'Sagamihara',
+        sttnAddr: '神奈川県相模原市中央区相模原一丁目',
+        ...JreYokohamaCommon
+    },
+    {
+        ekimeiKanj: '橋本',
+        ekimeiKana: 'はしもと',
+        ekimeiRoma: 'Hashimoto',
+        sttnAddr: '神奈川県相模原市緑区橋本六丁目',
+        ...JreYokohamaCommon
+    },
+    {
+        ekimeiKanj: '相原',
+        ekimeiKana: 'あいはら',
+        ekimeiRoma: 'Aihara',
+        sttnAddr: '東京都町田市相原町',
+        ...JreYokohamaCommon
+    },
+    {
+        ekimeiKanj: '八王子みなみ野',
+        ekimeiKana: 'はちおうじみなみの',
+        ekimeiRoma: 'Hachiōjiminamino',
+        sttnAddr: '東京都八王子市みなみ野一丁目',
+        ...JreYokohamaCommon
+    },
+    {
+        ekimeiKanj: '片倉',
+        ekimeiKana: 'かたくら',
+        ekimeiRoma: 'Katakura',
+        sttnAddr: '東京都八王子市片倉町',
+        ...JreYokohamaCommon
+    },
+    {
+        ekimeiKanj: '八王子',
+        ekimeiKana: 'はちおうじ',
+        ekimeiRoma: 'Hachiōji',
+        sttnAddr: '東京都八王子市旭町',
+        ...JreYokohamaCommon
+    },
+];
