3da3a0b393077e7385e7be0292eb857c85c047ab
miehorts <stroheimwt@yahoo.co.jp>
2023-08-06 11:13:04 +0900

    commit

--- main.js
+++ main.js
@@ -4,3 +4,17 @@ const mainH = document.querySelector('div.main-back').offsetHeight;
 const buttonH = document.querySelector('div.button-area').offsetHeight;
 information.style.height
 = (window.innerHeight - mainH - buttonH) + 'px';
+
+document.querySelector('#throw-dice').addEventListener('click',
+    (evt) => {
+        document.querySelector('div.main-back').animate(
+            [
+                { transform: 'translateX(0)' },
+                { transform: 'translateX(2000px)' }
+            ],
+            {
+                duration: 1000,
+            }
+        );
+    }
+);
