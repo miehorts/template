0c35a4938a9652b743b35080f59829a65d80a87c
miehorts <stroheimwt@yahoo.co.jp>
2023-08-06 13:50:44 +0900

    commit

--- main.js
+++ main.js
@@ -12,22 +12,14 @@ $('#throw-dice').on('click', (evt) => {
     setTimeout(() => { slideOut(); }      );
     setTimeout(() => { slideIn();  }, 1000);
     setTimeout(() => { slideOut(); }, 2000);
+    setTimeout(() => { slideIn();  }, 3000);
+    setTimeout(() => { slideOut(); }, 4000);
+    setTimeout(() => { slideIn();  }, 5000);
+    setTimeout(() => { slideOut(); }, 6000);
+    setTimeout(() => { slideIn();  }, 7000);
+    setTimeout(() => { slideOut(); }, 8000);
+    setTimeout(() => { slideIn();  }, 9000);
 });
-/*
-document.querySelector('div.main-fore').addEventListener('animationend',
-    (evt) => {
-        if (3 < ++slideCount) { return; }
-        if (inOut === 'in') {
-            slideOut();
-            inOut = 'out';
-        }
-        if (inOut === 'out') {
-            slideIn();
-            inOut = 'in';
-        }
-    }
-);
-*/
 
 function slideIn() {
     document.querySelector('div.main-fore').animate(
