6d8fa1c50a08fda5827c782b16708bf21a668da6
miehorts <stroheimwt@yahoo.co.jp>
2023-08-06 11:55:20 +0900

    commit

--- main.js
+++ main.js
@@ -5,25 +5,48 @@ const buttonH = document.querySelector('div.button-area').offsetHeight;
 information.style.height
 = (window.innerHeight - mainH - buttonH) + 'px';
 
+let inOut;
+
 document.querySelector('#throw-dice').addEventListener('click',
     (evt) => {
-        document.querySelector('div.main-fore').animate(
-            [
-                { transform: 'translateX(0)' },
-                { transform: 'translateX(-2000px)' }
-            ],
-            {
-                duration: 1000,
-            }
-        );
-        document.querySelector('div.main-fore').animate(
-            [
-                { transform: 'translateX(2000px)' },
-                { transform: 'translateX(0)' },
-            ],
-            {
-                duration: 1000,
-            }
-        );
+        slideOut();
+        inOut = 'out';
     }
 );
+
+document.querySelector('#throw-dice').addEventListener('animationend',
+    (evt) => {
+        if (inOut === 'in') {
+            slideOut();
+            inOut = 'out';
+        }
+        if (inOut === 'out') {
+            slideIn();
+            inOut = 'in';
+        }
+    }
+);
+
+function slideIn() {
+    document.querySelector('div.main-fore').animate(
+        [
+            { transform: 'translateX(2000px)' },
+            { transform: 'translateX(0)' },
+        ],
+        {
+            duration: 1000,
+        }
+    );
+}
+
+function slideOut() {
+    document.querySelector('div.main-fore').animate(
+        [
+            { transform: 'translateX(0)' },
+            { transform: 'translateX(-2000px)' }
+        ],
+        {
+            duration: 1000,
+        }
+    );
+}
