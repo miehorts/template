630157ac5db4f7ae7f3890186fdc64e7d71d12fb
miehorts <stroheimwt@yahoo.co.jp>
2023-08-10 16:23:16 +0900

    commit

---
+++ ekiData/JreSaikyou.js
@@ -0,0 +1,140 @@
+const JreSaikyouCommon = {
+    compName: '東日本旅客鉄道',
+    lineName: '埼京線',
+}
+
+const JreSaikyou = [
+    {
+        ekimeiKanj: '大崎',
+        ekimeiKana: 'おおさき',
+        ekimeiRoma: 'Ōsaki',
+        sttnAddr: '東京都品川区大崎一丁目',
+        ...JreSaikyouCommon
+    },
+    {
+        ekimeiKanj: '恵比寿',
+        ekimeiKana: 'えびす',
+        ekimeiRoma: 'Ebisu',
+        sttnAddr: '東京都渋谷区恵比寿南一丁目',
+        ...JreSaikyouCommon
+    },
+    {
+        ekimeiKanj: '渋谷',
+        ekimeiKana: 'しぶや',
+        ekimeiRoma: 'Shibuya',
+        sttnAddr: '東京都渋谷区',
+        ...JreSaikyouCommon
+    },
+    {
+        ekimeiKanj: '新宿',
+        ekimeiKana: 'しんじゅく',
+        ekimeiRoma: 'Shinjuku',
+        sttnAddr: '東京都新宿区・渋谷区',
+        ...JreSaikyouCommon
+    },
+    {
+        ekimeiKanj: '池袋',
+        ekimeiKana: 'いけぶくろ',
+        ekimeiRoma: 'Ikebukuro',
+        sttnAddr: '東京都豊島区',
+        ...JreSaikyouCommon
+    },
+    {
+        ekimeiKanj: '板橋',
+        ekimeiKana: 'いたばし',
+        ekimeiRoma: 'Itabashi',
+        sttnAddr: '東京都北区滝野川七丁目',
+        ...JreSaikyouCommon
+    },
+    {
+        ekimeiKanj: '十条',
+        ekimeiKana: 'じゅうじょう',
+        ekimeiRoma: 'Jūjō',
+        sttnAddr: '東京都北区上十条一丁目',
+        ...JreSaikyouCommon
+    },
+    {
+        ekimeiKanj: '赤羽',
+        ekimeiKana: 'あかばね',
+        ekimeiRoma: 'Akabane',
+        sttnAddr: '東京都北区赤羽一丁目',
+        ...JreSaikyouCommon
+    },
+    {
+        ekimeiKanj: '北赤羽',
+        ekimeiKana: 'きたあかばね',
+        ekimeiRoma: 'Kita-Akabane',
+        sttnAddr: '東京都北区赤羽北二丁目',
+        ...JreSaikyouCommon
+    },
+    {
+        ekimeiKanj: '浮間舟渡',
+        ekimeiKana: 'うきまふなど',
+        ekimeiRoma: 'Ukimafunado',
+        sttnAddr: '東京都北区浮間四丁目',
+        ...JreSaikyouCommon
+    },
+    {
+        ekimeiKanj: '戸田公園',
+        ekimeiKana: 'とだこうえん',
+        ekimeiRoma: 'Toda-Kōen',
+        sttnAddr: '埼玉県戸田市本町四丁目',
+        ...JreSaikyouCommon
+    },
+    {
+        ekimeiKanj: '戸田',
+        ekimeiKana: 'とだ',
+        ekimeiRoma: 'Toda',
+        sttnAddr: '埼玉県戸田市大字新曽字柳原',
+        ...JreSaikyouCommon
+    },
+    {
+        ekimeiKanj: '北戸田',
+        ekimeiKana: 'きたとだ',
+        ekimeiRoma: 'Kita-Toda',
+        sttnAddr: '埼玉県戸田市大字新曽字芦原',
+        ...JreSaikyouCommon
+    },
+    {
+        ekimeiKanj: '武蔵浦和',
+        ekimeiKana: 'むさしうらわ',
+        ekimeiRoma: 'Musashi-Urawa',
+        sttnAddr: '埼玉県さいたま市南区別所七丁目',
+        ...JreSaikyouCommon
+    },
+    {
+        ekimeiKanj: '中浦和',
+        ekimeiKana: 'なかうらわ',
+        ekimeiRoma: 'Naka-Urawa',
+        sttnAddr: '埼玉県さいたま市南区鹿手袋一丁目',
+        ...JreSaikyouCommon
+    },
+    {
+        ekimeiKanj: '南与野',
+        ekimeiKana: 'みなみよの',
+        ekimeiRoma: 'Minami-Yono',
+        sttnAddr: '埼玉県さいたま市中央区鈴谷二丁目',
+        ...JreSaikyouCommon
+    },
+    {
+        ekimeiKanj: '与野本町',
+        ekimeiKana: 'よのほんまち',
+        ekimeiRoma: 'Yono-Hommachi',
+        sttnAddr: '埼玉県さいたま市中央区本町東二丁目',
+        ...JreSaikyouCommon
+    },
+    {
+        ekimeiKanj: '北与野',
+        ekimeiKana: 'きたよの',
+        ekimeiRoma: 'Kita-Yono',
+        sttnAddr: '埼玉県さいたま市中央区上落合二丁目',
+        ...JreSaikyouCommon
+    },
+    {
+        ekimeiKanj: '大宮',
+        ekimeiKana: 'おおみや',
+        ekimeiRoma: 'Ōmiya',
+        sttnAddr: '埼玉県さいたま市大宮区錦町',
+        ...JreSaikyouCommon
+    },
+];
--- main.js
+++ main.js
@@ -13,6 +13,7 @@ const ekiList = [
     ...JreNanbu,
     ...JreMusashino,
     ...JreKeiyou,
+    ...JreSaikyou,
 ];
 
 $('#throw-dice').on('click', (evt) => {
