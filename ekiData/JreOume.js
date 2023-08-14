23751b519c24e08ff9b4ff9aaf03bd90d3b98700
miehorts <stroheimwt@yahoo.co.jp>
2023-08-14 16:45:37 +0900

    commit

--- ekiData/JreOume.js
+++ ekiData/JreOume.js
@@ -9,174 +9,174 @@ const JreOume = [
         ekimeiKana: 'たちかわ',
         ekimeiRoma: 'Tachikawa',
         sttnAddr: '東京都立川市曙町二丁目',
-        ...JreNegishiCommon
+        ...JreOumeCommon
     },
     {
         ekimeiKanj: '西立川',
         ekimeiKana: 'にしたちかわ',
         ekimeiRoma: 'Nishi-Tachikawa',
         sttnAddr: '東京都立川市富士見町一丁目',
-        ...JreNegishiCommon
+        ...JreOumeCommon
     },
     {
         ekimeiKanj: '東中神',
         ekimeiKana: 'ひがしなかがみ',
         ekimeiRoma: 'Higashi-Nakagami',
         sttnAddr: '東京都昭島市玉川町一丁目',
-        ...JreNegishiCommon
+        ...JreOumeCommon
     },
     {
         ekimeiKanj: '中神',
         ekimeiKana: 'なかがみ',
         ekimeiRoma: 'Nakagami',
         sttnAddr: '東京都昭島市朝日町一丁目',
-        ...JreNegishiCommon
+        ...JreOumeCommon
     },
     {
         ekimeiKanj: '昭島',
         ekimeiKana: 'あきしま',
         ekimeiRoma: 'Akishima',
         sttnAddr: '東京都昭島市昭和町二丁目',
-        ...JreNegishiCommon
+        ...JreOumeCommon
     },
     {
         ekimeiKanj: '拝島',
         ekimeiKana: 'はいじま',
         ekimeiRoma: 'Haijima',
         sttnAddr: '東京都昭島市松原町四丁目14-4',
-        ...JreNegishiCommon
+        ...JreOumeCommon
     },
     {
         ekimeiKanj: '牛浜',
         ekimeiKana: 'うしはま',
         ekimeiRoma: 'Ushihama',
         sttnAddr: '東京都福生市牛浜',
-        ...JreNegishiCommon
+        ...JreOumeCommon
     },
     {
         ekimeiKanj: '福生',
         ekimeiKana: 'ふっさ',
         ekimeiRoma: 'Fussa',
         sttnAddr: '東京都福生市本町',
-        ...JreNegishiCommon
+        ...JreOumeCommon
     },
     {
         ekimeiKanj: '羽村',
         ekimeiKana: 'はむら',
         ekimeiRoma: 'Hamura',
         sttnAddr: '東京都羽村市羽東一丁目',
-        ...JreNegishiCommon
+        ...JreOumeCommon
     },
     {
         ekimeiKanj: '小作',
         ekimeiKana: 'おざく',
         ekimeiRoma: 'Ozaku',
         sttnAddr: '東京都羽村市小作台五丁目',
-        ...JreNegishiCommon
+        ...JreOumeCommon
     },
     {
         ekimeiKanj: '河辺',
         ekimeiKana: 'かべ',
         ekimeiRoma: 'Kabe',
         sttnAddr: '東京都青梅市河辺町五丁目',
-        ...JreNegishiCommon
+        ...JreOumeCommon
     },
     {
         ekimeiKanj: '東青梅',
         ekimeiKana: 'ひがしおうめ',
         ekimeiRoma: 'Higashi-Ōme',
         sttnAddr: '東京都青梅市東青梅一丁目',
-        ...JreNegishiCommon
+        ...JreOumeCommon
     },
     {
         ekimeiKanj: '青梅',
         ekimeiKana: 'おうめ',
         ekimeiRoma: 'Ōme',
         sttnAddr: '東京都青梅市本町',
-        ...JreNegishiCommon
+        ...JreOumeCommon
     },
     {
         ekimeiKanj: '宮ノ平',
         ekimeiKana: 'みやのひら',
         ekimeiRoma: 'Miyanohira',
         sttnAddr: '東京都青梅市日向和田二丁目',
-        ...JreNegishiCommon
+        ...JreOumeCommon
     },
     {
         ekimeiKanj: '日向和田',
         ekimeiKana: 'ひなたわだ',
         ekimeiRoma: 'Hinatawada',
         sttnAddr: '東京都青梅市日向和田三丁目',
-        ...JreNegishiCommon
+        ...JreOumeCommon
     },
     {
         ekimeiKanj: '石神前',
         ekimeiKana: 'いしがみまえ',
         ekimeiRoma: 'Ishigamimae',
         sttnAddr: '東京都青梅市二俣尾一丁目',
-        ...JreNegishiCommon
+        ...JreOumeCommon
     },
     {
         ekimeiKanj: '二俣尾',
         ekimeiKana: 'ふたまたお',
         ekimeiRoma: 'Futamatao',
         sttnAddr: '東京都青梅市二俣尾四丁目',
-        ...JreNegishiCommon
+        ...JreOumeCommon
     },
     {
         ekimeiKanj: '軍畑',
         ekimeiKana: 'いくさばた',
         ekimeiRoma: 'Ikusabata',
         sttnAddr: '東京都青梅市沢井1丁目',
-        ...JreNegishiCommon
+        ...JreOumeCommon
     },
     {
         ekimeiKanj: '沢井',
         ekimeiKana: 'さわい',
         ekimeiRoma: 'Sawai',
         sttnAddr: '東京都青梅市沢井二丁目',
-        ...JreNegishiCommon
+        ...JreOumeCommon
     },
     {
         ekimeiKanj: '御嶽',
         ekimeiKana: 'みたけ',
         ekimeiRoma: 'Mitake',
         sttnAddr: '東京都青梅市御岳本町',
-        ...JreNegishiCommon
+        ...JreOumeCommon
     },
     {
         ekimeiKanj: '川井',
         ekimeiKana: 'かわい',
         ekimeiRoma: 'Kawai',
         sttnAddr: '東京都西多摩郡奥多摩町川井',
-        ...JreNegishiCommon
+        ...JreOumeCommon
     },
     {
         ekimeiKanj: '古里',
         ekimeiKana: 'こり',
         ekimeiRoma: 'Kori',
         sttnAddr: '東京都西多摩郡奥多摩町小丹波',
-        ...JreNegishiCommon
+        ...JreOumeCommon
     },
     {
         ekimeiKanj: '鳩ノ巣',
         ekimeiKana: 'はとのす',
         ekimeiRoma: 'Hatonosu',
         sttnAddr: '東京都西多摩郡奥多摩町棚沢',
-        ...JreNegishiCommon
+        ...JreOumeCommon
     },
     {
         ekimeiKanj: '白丸',
         ekimeiKana: 'しろまる',
         ekimeiRoma: 'Shiromaru',
         sttnAddr: '東京都西多摩郡奥多摩町白丸',
-        ...JreNegishiCommon
+        ...JreOumeCommon
     },
     {
         ekimeiKanj: '奥多摩',
         ekimeiKana: 'おくたま',
         ekimeiRoma: 'Oku-Tama',
         sttnAddr: '東京都西多摩郡奥多摩町氷川',
-        ...JreNegishiCommon
+        ...JreOumeCommon
     },
 ];
