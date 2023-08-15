ae0fa36dddf3fed915b779139fbe017361a4fc0b
miehorts <stroheimwt@yahoo.co.jp>
2023-08-15 18:01:52 +0900

    commit

---
+++ ekiData/JreSoutetsujrchokutsuu.js
@@ -0,0 +1,56 @@
+const JreSoutetsujrchokutsuuCommon = {
+    compName: 'JR東日本',
+    lineName: '相鉄・JR直通線',
+}
+
+const JreSoutetsujrchokutsuu = [
+    {
+        ekimeiKanj: '新宿',
+        ekimeiKana: 'しんじゅく',
+        ekimeiRoma: 'Shinjuku',
+        sttnAddr: '東京都新宿区新宿三丁目38-1',
+        ...JreSoutetsujrchokutsuuCommon
+    },
+    {
+        ekimeiKanj: '渋谷',
+        ekimeiKana: 'しぶや',
+        ekimeiRoma: 'Shibuya',
+        sttnAddr: '東京都渋谷区道玄坂一丁目1番1号',
+        ...JreSoutetsujrchokutsuuCommon
+    },
+    {
+        ekimeiKanj: '恵比寿',
+        ekimeiKana: 'えびす',
+        ekimeiRoma: 'Ebisu',
+        sttnAddr: '東京都渋谷区恵比寿南一丁目5-5',
+        ...JreSoutetsujrchokutsuuCommon
+    },
+    {
+        ekimeiKanj: '大崎',
+        ekimeiKana: 'おおさき',
+        ekimeiRoma: 'Ōsaki',
+        sttnAddr: '東京都品川区大崎一丁目21-4',
+        ...JreSoutetsujrchokutsuuCommon
+    },
+    {
+        ekimeiKanj: '西大井',
+        ekimeiKana: 'にしおおい',
+        ekimeiRoma: 'Nishi-Ōi',
+        sttnAddr: '東京都品川区西大井一丁目3-2',
+        ...JreSoutetsujrchokutsuuCommon
+    },
+    {
+        ekimeiKanj: '武蔵小杉',
+        ekimeiKana: 'むさしこすぎ',
+        ekimeiRoma: 'Musashi-Kosugi',
+        sttnAddr: '川崎市中原区新丸子東三丁目1111番地',
+        ...JreSoutetsujrchokutsuuCommon
+    },
+    {
+        ekimeiKanj: '羽沢横浜国大',
+        ekimeiKana: 'はざわ よこはまこくだい',
+        ekimeiRoma: 'Hazawa yokohama-kokudai',
+        sttnAddr: '横浜市神奈川区羽沢南二丁目471-3',
+        ...JreSoutetsujrchokutsuuCommon
+    },
+];
