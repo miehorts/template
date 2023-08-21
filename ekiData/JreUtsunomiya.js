c1528276416689ef2354c106f60f78e922bc7058
miehorts <stroheimwt@yahoo.co.jp>
2023-08-21 18:51:46 +0900

    commit

---
+++ ekiData/JreUtsunomiya.js
@@ -0,0 +1,112 @@
+const JreUtsunomiyaCommon = {
+    ...compNameJre,
+    lineName: '宇都宮線',
+}
+
+const JreUtsunomiya = [
+    {
+        ekimeiKanj: '東京',
+        ekimeiKana: 'とうきょう',
+        ekimeiRoma: 'Tōkyō',
+        sttnAddr: '東京都千代田区丸の内一丁目9-1',
+        ...JreUtsunomiyaCommon
+    },
+    {
+        ekimeiKanj: '上野',
+        ekimeiKana: 'うえの',
+        ekimeiRoma: 'Ueno',
+        sttnAddr: '東京都台東区上野七丁目1-1',
+        ...JreUtsunomiyaCommon
+    },
+    {
+        ekimeiKanj: '尾久',
+        ekimeiKana: 'おく',
+        ekimeiRoma: 'Oku',
+        sttnAddr: '東京都北区昭和町一丁目2-16',
+        ...JreUtsunomiyaCommon
+    },
+    {
+        ekimeiKanj: '赤羽',
+        ekimeiKana: 'あかばね',
+        ekimeiRoma: 'Akabane',
+        sttnAddr: '東京都北区赤羽一丁目1-1',
+        ...JreUtsunomiyaCommon
+    },
+    {
+        ekimeiKanj: '浦和',
+        ekimeiKana: 'うらわ',
+        ekimeiRoma: 'Urawa',
+        sttnAddr: '埼玉県さいたま市浦和区高砂一丁目16-12',
+        ...JreUtsunomiyaCommon
+    },
+    {
+        ekimeiKanj: 'さいたま新都心',
+        ekimeiKana: 'さいたましんとしん',
+        ekimeiRoma: 'Saitama-Shintoshin',
+        sttnAddr: '埼玉県さいたま市大宮区吉敷町四丁目261-1',
+        ...JreUtsunomiyaCommon
+    },
+    {
+        ekimeiKanj: '大宮',
+        ekimeiKana: 'おおみや',
+        ekimeiRoma: 'Ōmiya',
+        sttnAddr: '埼玉県さいたま市大宮区錦町630',
+        ...JreUtsunomiyaCommon
+    },
+    {
+        ekimeiKanj: '',
+        ekimeiKana: '',
+        ekimeiRoma: '',
+        sttnAddr: '',
+        ...JreUtsunomiyaCommon
+    },
+    {
+        ekimeiKanj: '',
+        ekimeiKana: '',
+        ekimeiRoma: '',
+        sttnAddr: '',
+        ...JreUtsunomiyaCommon
+    },
+    {
+        ekimeiKanj: '',
+        ekimeiKana: '',
+        ekimeiRoma: '',
+        sttnAddr: '',
+        ...JreUtsunomiyaCommon
+    },
+    {
+        ekimeiKanj: '',
+        ekimeiKana: '',
+        ekimeiRoma: '',
+        sttnAddr: '',
+        ...JreUtsunomiyaCommon
+    },
+    {
+        ekimeiKanj: '',
+        ekimeiKana: '',
+        ekimeiRoma: '',
+        sttnAddr: '',
+        ...JreUtsunomiyaCommon
+    },
+    {
+        ekimeiKanj: '',
+        ekimeiKana: '',
+        ekimeiRoma: '',
+        sttnAddr: '',
+        ...JreUtsunomiyaCommon
+    },
+    {
+        ekimeiKanj: '',
+        ekimeiKana: '',
+        ekimeiRoma: '',
+        sttnAddr: '',
+        ...JreUtsunomiyaCommon
+    },
+    {
+        ekimeiKanj: '',
+        ekimeiKana: '',
+        ekimeiRoma: '',
+        sttnAddr: '',
+        ...JreUtsunomiyaCommon
+    },
+];
