089e849b07d6c32140abb916419bd823ca33ddde
miehorts <stroheimwt@yahoo.co.jp>
2023-08-21 11:48:39 +0900

    commit

--- ekiData/JreChuuousoubukankou.js
+++ ekiData/JreChuuousoubukankou.js
@@ -1,6 +1,7 @@
 const JreChuuousoubukankouCommon = {
     ...compNameJre,
-    lineName: '中央・総武緩行線',
+    lineName1: { lineName: '総武線（各駅停車）' },
+    lineName2: { lineName: '中央・総武線（各駅停車）' },
 };
 
 const JreChuuousoubukankou = [
@@ -9,272 +10,311 @@ const JreChuuousoubukankou = [
         ekimeiKana: 'ちば',
         ekimeiRoma: 'Chiba',
         sttnAddr: '千葉県千葉市中央区新千葉一丁目',
-        ...JreChuuousoubukankouCommon
+        ...compNameJre,
+        ...JreChuuousoubukankouCommon.lineName1,
     },
     {
         ekimeiKanj: '西千葉',
         ekimeiKana: 'にしちば',
         ekimeiRoma: 'Nishi-Chiba',
         sttnAddr: '千葉市中央区春日二丁目24-2',
-        ...JreChuuousoubukankouCommon
+        ...compNameJre,
+        ...JreChuuousoubukankouCommon.lineName1,
     },
     {
         ekimeiKanj: '稲毛',
         ekimeiKana: 'いなげ',
         ekimeiRoma: 'Inage',
         sttnAddr: '千葉市稲毛区稲毛東三丁目19-11',
-        ...JreChuuousoubukankouCommon
+        ...compNameJre,
+        ...JreChuuousoubukankouCommon.lineName1,
     },
     {
         ekimeiKanj: '新検見川',
         ekimeiKana: 'しんけみがわ',
         ekimeiRoma: 'Shin-Kemigawa',
         sttnAddr: '千葉市花見川区南花園二丁目1-1',
-        ...JreChuuousoubukankouCommon
+        ...compNameJre,
+        ...JreChuuousoubukankouCommon.lineName1,
     },
     {
         ekimeiKanj: '幕張',
         ekimeiKana: 'まくはり',
         ekimeiRoma: 'Makuhari',
         sttnAddr: '千葉市花見川区幕張町五丁目121',
-        ...JreChuuousoubukankouCommon
+        ...compNameJre,
+        ...JreChuuousoubukankouCommon.lineName1,
     },
     {
         ekimeiKanj: '幕張本郷',
         ekimeiKana: 'まくはりほんごう',
         ekimeiRoma: 'Makuharihongō',
         sttnAddr: '千葉市花見川区幕張本郷一丁目1-1',
-        ...JreChuuousoubukankouCommon
+        ...compNameJre,
+        ...JreChuuousoubukankouCommon.lineName1,
     },
     {
         ekimeiKanj: '津田沼',
         ekimeiKana: 'つだぬま',
         ekimeiRoma: 'Tsudanuma',
         sttnAddr: '千葉県習志野市津田沼一丁目1-1',
-        ...JreChuuousoubukankouCommon
+        ...compNameJre,
+        ...JreChuuousoubukankouCommon.lineName1,
     },
     {
         ekimeiKanj: '東船橋',
         ekimeiKana: 'ひがしふなばし',
         ekimeiRoma: 'Higashi-Funabashi',
         sttnAddr: '千葉県船橋市東船橋二丁目10-1',
-        ...JreChuuousoubukankouCommon
+        ...compNameJre,
+        ...JreChuuousoubukankouCommon.lineName1,
     },
     {
         ekimeiKanj: '船橋',
         ekimeiKana: 'ふなばし',
         ekimeiRoma: 'Funabashi',
         sttnAddr: '千葉県船橋市本町七丁目1',
-        ...JreChuuousoubukankouCommon
+        ...compNameJre,
+        ...JreChuuousoubukankouCommon.lineName1,
     },
     {
         ekimeiKanj: '西船橋',
         ekimeiKana: 'にしふなばし',
         ekimeiRoma: 'Nishi-Funabashi',
         sttnAddr: '千葉県船橋市西船四丁目27-7',
-        ...JreChuuousoubukankouCommon
+        ...compNameJre,
+        ...JreChuuousoubukankouCommon.lineName1,
     },
     {
         ekimeiKanj: '下総中山',
         ekimeiKana: 'しもうさなかやま',
         ekimeiRoma: 'Shimōsa-Nakayama',
         sttnAddr: '千葉県船橋市本中山二丁目17-37',
-        ...JreChuuousoubukankouCommon
+        ...compNameJre,
+        ...JreChuuousoubukankouCommon.lineName1,
     },
     {
         ekimeiKanj: '本八幡',
         ekimeiKana: 'もとやわた',
         ekimeiRoma: 'Moto-Yawata',
         sttnAddr: '千葉県市川市八幡二丁目17-1',
-        ...JreChuuousoubukankouCommon
+        ...compNameJre,
+        ...JreChuuousoubukankouCommon.lineName1,
     },
     {
         ekimeiKanj: '市川',
         ekimeiKana: 'いちかわ',
         ekimeiRoma: 'Ichikawa',
         sttnAddr: '千葉県市川市市川一丁目1-1',
-        ...JreChuuousoubukankouCommon
+        ...compNameJre,
+        ...JreChuuousoubukankouCommon.lineName1,
     },
     {
         ekimeiKanj: '小岩',
         ekimeiKana: 'こいわ',
         ekimeiRoma: 'Koiwa',
         sttnAddr: '東京都江戸川区南小岩七丁目24-15',
-        ...JreChuuousoubukankouCommon
+        ...compNameJre,
+        ...JreChuuousoubukankouCommon.lineName1,
     },
     {
         ekimeiKanj: '新小岩',
         ekimeiKana: 'しんこいわ',
         ekimeiRoma: 'Shin-Koiwa',
         sttnAddr: '東京都葛飾区新小岩一丁目45-1',
-        ...JreChuuousoubukankouCommon
+        ...compNameJre,
+        ...JreChuuousoubukankouCommon.lineName1,
     },
     {
         ekimeiKanj: '平井',
         ekimeiKana: 'ひらい',
         ekimeiRoma: 'Hirai',
         sttnAddr: '東京都江戸川区平井三丁目30-1',
-        ...JreChuuousoubukankouCommon
+        ...compNameJre,
+        ...JreChuuousoubukankouCommon.lineName1,
     },
     {
         ekimeiKanj: '亀戸',
         ekimeiKana: 'かめいど',
         ekimeiRoma: 'Kameido',
         sttnAddr: '東京都江東区亀戸五丁目1-1',
-        ...JreChuuousoubukankouCommon
+        ...compNameJre,
+        ...JreChuuousoubukankouCommon.lineName1,
     },
     {
         ekimeiKanj: '錦糸町',
         ekimeiKana: 'きんしちょう',
         ekimeiRoma: 'Kinshichō',
         sttnAddr: '東京都墨田区江東橋三丁目14-5',
-        ...JreChuuousoubukankouCommon
+        ...compNameJre,
+        ...JreChuuousoubukankouCommon.lineName1,
     },
     {
         ekimeiKanj: '両国',
         ekimeiKana: 'りょうごく',
         ekimeiRoma: 'Ryōgoku',
         sttnAddr: '東京都墨田区横網一丁目3-20',
-        ...JreChuuousoubukankouCommon
+        ...compNameJre,
+        ...JreChuuousoubukankouCommon.lineName1,
     },
     {
         ekimeiKanj: '浅草橋',
         ekimeiKana: 'あさくさばし',
         ekimeiRoma: 'Asakusabashi',
         sttnAddr: '東京都台東区浅草橋一丁目18番1号',
-        ...JreChuuousoubukankouCommon
+        ...compNameJre,
+        ...JreChuuousoubukankouCommon.lineName1,
     },
     {
         ekimeiKanj: '秋葉原',
         ekimeiKana: 'あきはばら',
         ekimeiRoma: 'Akihabara',
         sttnAddr: '東京都千代田区外神田一丁目17-6',
-        ...JreChuuousoubukankouCommon
+        ...compNameJre,
+        ...JreChuuousoubukankouCommon.lineName2,
     },
     {
         ekimeiKanj: '御茶ノ水',
         ekimeiKana: 'おちゃのみず',
         ekimeiRoma: 'Ochanomizu',
         sttnAddr: '東京都千代田区神田駿河台二丁目6-1',
-        ...JreChuuousoubukankouCommon
+        ...compNameJre,
+        ...JreChuuousoubukankouCommon.lineName2,
     },
     {
         ekimeiKanj: '水道橋',
         ekimeiKana: 'すいどうばし',
         ekimeiRoma: 'Suidōbashi',
         sttnAddr: '東京都千代田区神田三崎町二丁目22-1',
-        ...JreChuuousoubukankouCommon
+        ...compNameJre,
+        ...JreChuuousoubukankouCommon.lineName2,
     },
     {
         ekimeiKanj: '飯田橋',
         ekimeiKana: 'いいだばし',
         ekimeiRoma: 'Iidabashi',
         sttnAddr: '東京都千代田区飯田橋四丁目10-2',
-        ...JreChuuousoubukankouCommon
+        ...compNameJre,
+        ...JreChuuousoubukankouCommon.lineName2,
     },
     {
         ekimeiKanj: '市ケ谷',
         ekimeiKana: 'いちがや',
         ekimeiRoma: 'Ichigaya',
         sttnAddr: '東京都千代田区五番町2-1',
-        ...JreChuuousoubukankouCommon
+        ...compNameJre,
+        ...JreChuuousoubukankouCommon.lineName2,
     },
     {
         ekimeiKanj: '四ツ谷',
         ekimeiKana: 'よつや',
         ekimeiRoma: 'Yotsuya',
         sttnAddr: '東京都新宿区四谷一丁目無番地',
-        ...JreChuuousoubukankouCommon
+        ...compNameJre,
+        ...JreChuuousoubukankouCommon.lineName2,
     },
     {
         ekimeiKanj: '信濃町',
         ekimeiKana: 'しなのまち',
         ekimeiRoma: 'Shinanomachi',
         sttnAddr: '東京都新宿区信濃町34',
-        ...JreChuuousoubukankouCommon
+        ...compNameJre,
+        ...JreChuuousoubukankouCommon.lineName2,
     },
     {
         ekimeiKanj: '千駄ケ谷',
         ekimeiKana: 'せんだがや',
         ekimeiRoma: 'Sendagaya',
         sttnAddr: '東京都渋谷区千駄ヶ谷一丁目35-10',
-        ...JreChuuousoubukankouCommon
+        ...compNameJre,
+        ...JreChuuousoubukankouCommon.lineName2,
     },
     {
         ekimeiKanj: '代々木',
         ekimeiKana: 'よよぎ',
         ekimeiRoma: 'Yoyogi',
         sttnAddr: '東京都渋谷区代々木一丁目34-1',
-        ...JreChuuousoubukankouCommon
+        ...compNameJre,
+        ...JreChuuousoubukankouCommon.lineName2,
     },
     {
         ekimeiKanj: '新宿',
         ekimeiKana: 'しんじゅく',
         ekimeiRoma: 'Shinjuku',
         sttnAddr: '東京都新宿区新宿三丁目38-1',
-        ...JreChuuousoubukankouCommon
+        ...compNameJre,
+        ...JreChuuousoubukankouCommon.lineName2,
     },
     {
         ekimeiKanj: '大久保',
         ekimeiKana: 'おおくぼ',
         ekimeiRoma: 'Ōkubo',
         sttnAddr: '東京都新宿区百人町一丁目17-1',
-        ...JreChuuousoubukankouCommon
+        ...compNameJre,
+        ...JreChuuousoubukankouCommon.lineName2,
     },
     {
         ekimeiKanj: '東中野',
         ekimeiKana: 'ひがしなかの',
         ekimeiRoma: 'Higashi-Nakano',
         sttnAddr: '東京都中野区東中野四丁目1-12',
-        ...JreChuuousoubukankouCommon
+        ...compNameJre,
+        ...JreChuuousoubukankouCommon.lineName2,
     },
     {
         ekimeiKanj: '中野',
         ekimeiKana: 'なかの',
         ekimeiRoma: 'Nakano',
         sttnAddr: '東京都中野区中野五丁目31-1',
-        ...JreChuuousoubukankouCommon
+        ...compNameJre,
+        ...JreChuuousoubukankouCommon.lineName2,
     },
     {
         ekimeiKanj: '高円寺',
         ekimeiKana: 'こうえんじ',
         ekimeiRoma: 'Kōenji',
         sttnAddr: '東京都杉並区高円寺南四丁目48-2',
-        ...JreChuuousoubukankouCommon
+        ...compNameJre,
+        ...JreChuuousoubukankouCommon.lineName2,
     },
     {
         ekimeiKanj: '阿佐ケ谷',
         ekimeiKana: 'あさがや',
         ekimeiRoma: 'Asagaya',
         sttnAddr: '東京都杉並区阿佐谷南三丁目36-2',
-        ...JreChuuousoubukankouCommon
+        ...compNameJre,
+        ...JreChuuousoubukankouCommon.lineName2,
     },
     {
         ekimeiKanj: '荻窪',
         ekimeiKana: 'おぎくぼ',
         ekimeiRoma: 'Ogikubo',
         sttnAddr: '東京都杉並区上荻一丁目7-1',
-        ...JreChuuousoubukankouCommon
+        ...compNameJre,
+        ...JreChuuousoubukankouCommon.lineName2,
     },
     {
         ekimeiKanj: '西荻窪',
         ekimeiKana: 'にしおぎくぼ',
         ekimeiRoma: 'Nishi-Ogikubo',
         sttnAddr: '東京都杉並区西荻南三丁目25-1',
-        ...JreChuuousoubukankouCommon
+        ...compNameJre,
+        ...JreChuuousoubukankouCommon.lineName2,
     },
     {
         ekimeiKanj: '吉祥寺',
         ekimeiKana: 'きちじょうじ',
         ekimeiRoma: 'Kichijōji',
         sttnAddr: '東京都武蔵野市吉祥寺南町一丁目1番24号',
-        ...JreChuuousoubukankouCommon
+        ...compNameJre,
+        ...JreChuuousoubukankouCommon.lineName2,
     },
     {
         ekimeiKanj: '三鷹',
         ekimeiKana: 'みたか',
         ekimeiRoma: 'Mitaka',
         sttnAddr: '東京都三鷹市下連雀三丁目46番1号',
-        ...JreChuuousoubukankouCommon
+        ...compNameJre,
+        ...JreChuuousoubukankouCommon.lineName2,
     },
 ];
