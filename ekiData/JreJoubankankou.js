6675ca8f691dcbf80dbb5c6707256a64c1a33ada
miehorts <stroheimwt@yahoo.co.jp>
2023-08-25 13:37:14 +0900

    commit

--- ekiData/JreJoubankankou.js
+++ ekiData/JreJoubankankou.js
@@ -11,7 +11,7 @@ const JreJoubankankou = [
         ekimeiRoma: 'Ayase',
         sttnAddr: '東京都足立区綾瀬三丁目1-1',
         ...compNameJre,
-        ...JreJoubankankou.lineName1,
+        ...JreJoubankankouCommon.lineName1,
     },
     {
         ekimeiKanj: '亀有',
@@ -19,7 +19,7 @@ const JreJoubankankou = [
         ekimeiRoma: 'Kameari',
         sttnAddr: '東京都葛飾区亀有三丁目25-1',
         ...compNameJre,
-        ...JreJoubankankou.lineName1,
+        ...JreJoubankankouCommon.lineName1,
     },
     {
         ekimeiKanj: '金町',
@@ -27,7 +27,7 @@ const JreJoubankankou = [
         ekimeiRoma: 'Kanamachi',
         sttnAddr: '東京都葛飾区金町六丁目4-1',
         ...compNameJre,
-        ...JreJoubankankou.lineName1,
+        ...JreJoubankankouCommon.lineName1,
     },
     {
         ekimeiKanj: '松戸',
@@ -35,7 +35,7 @@ const JreJoubankankou = [
         ekimeiRoma: 'Matsudo',
         sttnAddr: '千葉県松戸市松戸1181',
         ...compNameJre,
-        ...JreJoubankankou.lineName1,
+        ...JreJoubankankouCommon.lineName1,
     },
     {
         ekimeiKanj: '北松戸',
@@ -43,7 +43,7 @@ const JreJoubankankou = [
         ekimeiRoma: 'Kita-Matsudo',
         sttnAddr: '千葉県松戸市上本郷905',
         ...compNameJre,
-        ...JreJoubankankou.lineName1,
+        ...JreJoubankankouCommon.lineName1,
     },
     {
         ekimeiKanj: '馬橋',
@@ -51,7 +51,7 @@ const JreJoubankankou = [
         ekimeiRoma: 'Mabashi',
         sttnAddr: '千葉県松戸市馬橋121',
         ...compNameJre,
-        ...JreJoubankankou.lineName1,
+        ...JreJoubankankouCommon.lineName1,
     },
     {
         ekimeiKanj: '新松戸',
@@ -59,7 +59,7 @@ const JreJoubankankou = [
         ekimeiRoma: 'Shim-Matsudo',
         sttnAddr: '千葉県松戸市幸谷571-3',
         ...compNameJre,
-        ...JreJoubankankou.lineName1,
+        ...JreJoubankankouCommon.lineName1,
     },
     {
         ekimeiKanj: '北小金',
@@ -67,7 +67,7 @@ const JreJoubankankou = [
         ekimeiRoma: 'Kita-Kogane',
         sttnAddr: '千葉県松戸市小金8-3',
         ...compNameJre,
-        ...JreJoubankankou.lineName1,
+        ...JreJoubankankouCommon.lineName1,
     },
     {
         ekimeiKanj: '南柏',
@@ -75,7 +75,7 @@ const JreJoubankankou = [
         ekimeiRoma: 'Minami-Kashiwa',
         sttnAddr: '千葉県柏市南柏一丁目1-1',
         ...compNameJre,
-        ...JreJoubankankou.lineName1,
+        ...JreJoubankankouCommon.lineName1,
     },
     {
         ekimeiKanj: '柏',
@@ -83,7 +83,7 @@ const JreJoubankankou = [
         ekimeiRoma: 'Kashiwa',
         sttnAddr: '千葉県柏市柏一丁目1-1',
         ...compNameJre,
-        ...JreJoubankankou.lineName1,
+        ...JreJoubankankouCommon.lineName1,
     },
     {
         ekimeiKanj: '北柏',
@@ -91,7 +91,7 @@ const JreJoubankankou = [
         ekimeiRoma: 'Kita-Kashiwa',
         sttnAddr: '千葉県柏市根戸字中馬場1901',
         ...compNameJre,
-        ...JreJoubankankou.lineName1,
+        ...JreJoubankankouCommon.lineName1,
     },
     {
         ekimeiKanj: '我孫子',
@@ -99,7 +99,7 @@ const JreJoubankankou = [
         ekimeiRoma: 'Abiko',
         sttnAddr: '千葉県我孫子市本町二丁目1-1',
         ...compNameJre,
-        ...JreJoubankankou.lineName1,
+        ...JreJoubankankouCommon.lineName1,
     },
     {
         ekimeiKanj: '天王台',
@@ -107,7 +107,7 @@ const JreJoubankankou = [
         ekimeiRoma: 'Tennōdai',
         sttnAddr: '千葉県我孫子市柴崎台一丁目19-1',
         ...compNameJre,
-        ...JreJoubankankou.lineName1,
+        ...JreJoubankankouCommon.lineName1,
     },
     {
         ekimeiKanj: '取手',
@@ -115,6 +115,6 @@ const JreJoubankankou = [
         ekimeiRoma: 'Toride',
         sttnAddr: '茨城県取手市中央町1-1',
         ...compNameJre,
-        ...JreJoubankankou.lineName1,
+        ...JreJoubankankouCommon.lineName1,
     },
 ];
---
+++ ekiData/JreJouetsu.js
@@ -0,0 +1,70 @@
+const JreJouetsuCommon = {
+    ...compNameJre,
+    lineName: '上越線',
+}
+
+const JreJouetsu = [
+    {
+        ekimeiKanj: '高崎',
+        ekimeiKana: 'たかさき',
+        ekimeiRoma: 'Takasaki',
+        sttnAddr: '群馬県高崎市八島町222',
+        ...JreJouetsuCommon
+    },
+    {
+        ekimeiKanj: '高崎問屋町',
+        ekimeiKana: 'たかさきとんやまち',
+        ekimeiRoma: 'Takasakitonyamachi',
+        sttnAddr: '群馬県高崎市貝沢町715',
+        ...JreJouetsuCommon
+    },
+    {
+        ekimeiKanj: '',
+        ekimeiKana: '',
+        ekimeiRoma: '',
+        sttnAddr: '',
+        ...JreJouetsuCommon
+    },
+    {
+        ekimeiKanj: '',
+        ekimeiKana: '',
+        ekimeiRoma: '',
+        sttnAddr: '',
+        ...JreJouetsuCommon
+    },
+    {
+        ekimeiKanj: '',
+        ekimeiKana: '',
+        ekimeiRoma: '',
+        sttnAddr: '',
+        ...JreJouetsuCommon
+    },
+    {
+        ekimeiKanj: '',
+        ekimeiKana: '',
+        ekimeiRoma: '',
+        sttnAddr: '',
+        ...JreJouetsuCommon
+    },
+    {
+        ekimeiKanj: '',
+        ekimeiKana: '',
+        ekimeiRoma: '',
+        sttnAddr: '',
+        ...JreJouetsuCommon
+    },
+    {
+        ekimeiKanj: '',
+        ekimeiKana: '',
+        ekimeiRoma: '',
+        sttnAddr: '',
+        ...JreJouetsuCommon
+    },
+    {
+        ekimeiKanj: '',
+        ekimeiKana: '',
+        ekimeiRoma: '',
+        sttnAddr: '',
+        ...JreJouetsuCommon
+    },
+];
