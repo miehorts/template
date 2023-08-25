dc98257a7ab5a3c1c9dbc411e0019973324fb2ab
miehorts <stroheimwt@yahoo.co.jp>
2023-08-25 15:49:02 +0900

    commit

--- ekiData/JreJouetsu.js
+++ ekiData/JreJouetsu.js
@@ -19,52 +19,241 @@ const JreJouetsu = [
         ...JreJouetsuCommon
     },
     {
-        ekimeiKanj: '',
-        ekimeiKana: '',
-        ekimeiRoma: '',
-        sttnAddr: '',
+        ekimeiKanj: '井野',
+        ekimeiKana: 'いの',
+        ekimeiRoma: 'Ino',
+        sttnAddr: '群馬県高崎市井野町103',
         ...JreJouetsuCommon
     },
     {
-        ekimeiKanj: '',
-        ekimeiKana: '',
-        ekimeiRoma: '',
-        sttnAddr: '',
+        ekimeiKanj: '新前橋',
+        ekimeiKana: 'しんまえばし',
+        ekimeiRoma: 'Shim-Maebashi',
+        sttnAddr: '群馬県前橋市古市町471',
         ...JreJouetsuCommon
     },
     {
-        ekimeiKanj: '',
-        ekimeiKana: '',
-        ekimeiRoma: '',
-        sttnAddr: '',
+        ekimeiKanj: '群馬総社',
+        ekimeiKana: 'ぐんまそうじゃ',
+        ekimeiRoma: 'Gumma-Sōja',
+        sttnAddr: '群馬県前橋市総社町植野588',
         ...JreJouetsuCommon
     },
     {
-        ekimeiKanj: '',
-        ekimeiKana: '',
-        ekimeiRoma: '',
-        sttnAddr: '',
+        ekimeiKanj: '八木原',
+        ekimeiKana: 'やぎはら',
+        ekimeiRoma: 'Yagihara',
+        sttnAddr: '群馬県渋川市八木原1145',
         ...JreJouetsuCommon
     },
     {
-        ekimeiKanj: '',
-        ekimeiKana: '',
-        ekimeiRoma: '',
-        sttnAddr: '',
+        ekimeiKanj: '渋川',
+        ekimeiKana: 'しぶかわ',
+        ekimeiRoma: 'Shibukawa',
+        sttnAddr: '群馬県渋川市渋川辰巳町1651-4',
         ...JreJouetsuCommon
     },
     {
-        ekimeiKanj: '',
-        ekimeiKana: '',
-        ekimeiRoma: '',
-        sttnAddr: '',
+        ekimeiKanj: '敷島',
+        ekimeiKana: 'しきしま',
+        ekimeiRoma: 'Shikishima',
+        sttnAddr: '群馬県渋川市赤城町敷島354-7',
         ...JreJouetsuCommon
     },
     {
-        ekimeiKanj: '',
-        ekimeiKana: '',
-        ekimeiRoma: '',
-        sttnAddr: '',
+        ekimeiKanj: '津久田',
+        ekimeiKana: 'つくだ',
+        ekimeiRoma: 'Tsukuda',
+        sttnAddr: '群馬県渋川市赤城町津久田2854-2',
+        ...JreJouetsuCommon
+    },
+    {
+        ekimeiKanj: '岩本',
+        ekimeiKana: 'いわもと',
+        ekimeiRoma: 'Iwamoto',
+        sttnAddr: '群馬県沼田市岩本町173',
+        ...JreJouetsuCommon
+    },
+    {
+        ekimeiKanj: '沼田',
+        ekimeiKana: 'ぬまた',
+        ekimeiRoma: 'Numata',
+        sttnAddr: '群馬県沼田市清水町3155',
+        ...JreJouetsuCommon
+    },
+    {
+        ekimeiKanj: '後閑',
+        ekimeiKana: 'ごかん',
+        ekimeiRoma: 'Gokan',
+        sttnAddr: '群馬県利根郡みなかみ町後閑1237',
+        ...JreJouetsuCommon
+    },
+    {
+        ekimeiKanj: '上牧',
+        ekimeiKana: 'かみもく',
+        ekimeiRoma: 'Kamimoku',
+        sttnAddr: '群馬県利根郡みなかみ町上牧2145',
+        ...JreJouetsuCommon
+    },
+    {
+        ekimeiKanj: '水上',
+        ekimeiKana: 'みなかみ',
+        ekimeiRoma: 'Minakami',
+        sttnAddr: '群馬県利根郡みなかみ町鹿野沢96',
+        ...JreJouetsuCommon
+    },
+    {
+        ekimeiKanj: '湯檜曽',
+        ekimeiKana: 'ゆびそ',
+        ekimeiRoma: 'Yubiso',
+        sttnAddr: '群馬県利根郡みなかみ町湯檜曽18-2',
+        ...JreJouetsuCommon
+    },
+    {
+        ekimeiKanj: '土合',
+        ekimeiKana: 'どあい',
+        ekimeiRoma: 'Doai',
+        sttnAddr: '群馬県利根郡みなかみ町湯檜曽218-2',
+        ...JreJouetsuCommon
+    },
+    {
+        ekimeiKanj: '土樽',
+        ekimeiKana: 'つちたる',
+        ekimeiRoma: 'Tsuchitaru',
+        sttnAddr: '新潟県南魚沼郡湯沢町大字土樽',
+        ...JreJouetsuCommon
+    },
+    {
+        ekimeiKanj: '越後中里',
+        ekimeiKana: 'えちごなかざと',
+        ekimeiRoma: 'Echigo-Nakazato',
+        sttnAddr: '新潟県南魚沼郡湯沢町大字土樽5135',
+        ...JreJouetsuCommon
+    },
+    {
+        ekimeiKanj: '岩原スキー場前',
+        ekimeiKana: 'いわっぱらすきーじょうまえ',
+        ekimeiRoma: 'Iwappara Skiing Ground',
+        sttnAddr: '新潟県南魚沼郡湯沢町大字土樽',
+        ...JreJouetsuCommon
+    },
+    {
+        ekimeiKanj: '越後湯沢',
+        ekimeiKana: 'えちごゆざわ',
+        ekimeiRoma: 'Echigo-Yuzawa',
+        sttnAddr: '新潟県南魚沼郡湯沢町大字湯沢主水2427-1',
+        ...JreJouetsuCommon
+    },
+    {
+        ekimeiKanj: '石打',
+        ekimeiKana: 'いしうち',
+        ekimeiRoma: 'Ishiuchi',
+        sttnAddr: '新潟県南魚沼市上野1',
+        ...JreJouetsuCommon
+    },
+    {
+        ekimeiKanj: '大沢',
+        ekimeiKana: 'おおさわ',
+        ekimeiRoma: 'Ōsawa',
+        sttnAddr: '新潟県南魚沼市大沢',
+        ...JreJouetsuCommon
+    },
+    {
+        ekimeiKanj: '上越国際スキー場前',
+        ekimeiKana: 'じょうえつこくさいすきーじょうまえ',
+        ekimeiRoma: 'Jōetsu International Skiing Ground',
+        sttnAddr: '新潟県南魚沼市樺野沢',
+        ...JreJouetsuCommon
+    },
+    {
+        ekimeiKanj: '塩沢',
+        ekimeiKana: 'しおざわ',
+        ekimeiRoma: 'Shiozawa',
+        sttnAddr: '新潟県南魚沼市塩沢1229-2',
+        ...JreJouetsuCommon
+    },
+    {
+        ekimeiKanj: '六日町',
+        ekimeiKana: 'むいかまち',
+        ekimeiRoma: 'Muikamachi',
+        sttnAddr: '新潟県南魚沼市六日町140番地2',
+        ...JreJouetsuCommon
+    },
+    {
+        ekimeiKanj: '五日町',
+        ekimeiKana: 'いつかまち',
+        ekimeiRoma: 'Itsukamachi',
+        sttnAddr: '新潟県南魚沼市五日町',
+        ...JreJouetsuCommon
+    },
+    {
+        ekimeiKanj: '浦佐',
+        ekimeiKana: 'うらさ',
+        ekimeiRoma: 'Urasa',
+        sttnAddr: '新潟県南魚沼市浦佐719-2',
+        ...JreJouetsuCommon
+    },
+    {
+        ekimeiKanj: '八色',
+        ekimeiKana: 'やいろ',
+        ekimeiRoma: 'Yairo',
+        sttnAddr: '新潟県南魚沼市五箇',
+        ...JreJouetsuCommon
+    },
+    {
+        ekimeiKanj: '小出',
+        ekimeiKana: 'こいで',
+        ekimeiRoma: 'Koide',
+        sttnAddr: '新潟県魚沼市四日町1315',
+        ...JreJouetsuCommon
+    },
+    {
+        ekimeiKanj: '越後堀之内',
+        ekimeiKana: 'えちごほりのうち',
+        ekimeiRoma: 'Echigo-Horinouchi',
+        sttnAddr: '新潟県魚沼市堀之内3860',
+        ...JreJouetsuCommon
+    },
+    {
+        ekimeiKanj: '北堀之内',
+        ekimeiKana: 'きたほりのうち',
+        ekimeiRoma: 'Kita-Horinouchi',
+        sttnAddr: '新潟県魚沼市下島',
+        ...JreJouetsuCommon
+    },
+    {
+        ekimeiKanj: '越後川口',
+        ekimeiKana: 'えちごかわぐち',
+        ekimeiRoma: 'Echigo-Kawaguchi',
+        sttnAddr: '新潟県長岡市東川口693-1',
+        ...JreJouetsuCommon
+    },
+    {
+        ekimeiKanj: '小千谷',
+        ekimeiKana: 'おぢや',
+        ekimeiRoma: 'Ojiya',
+        sttnAddr: '新潟県小千谷市東栄一丁目1-1',
+        ...JreJouetsuCommon
+    },
+    {
+        ekimeiKanj: '越後滝谷',
+        ekimeiKana: 'えちごたきや',
+        ekimeiRoma: 'Echigo-Takiya',
+        sttnAddr: '新潟県長岡市滝谷町402',
+        ...JreJouetsuCommon
+    },
+    {
+        ekimeiKanj: '宮内',
+        ekimeiKana: 'みやうち',
+        ekimeiRoma: 'Miyauchi',
+        sttnAddr: '新潟県長岡市宮内三丁目4番1号',
+        ...JreJouetsuCommon
+    },
+    {
+        ekimeiKanj: '長岡',
+        ekimeiKana: 'ながおか',
+        ekimeiRoma: 'Nagaoka',
+        sttnAddr: '新潟県長岡市城内町二丁目794-4',
         ...JreJouetsuCommon
     },
 ];
