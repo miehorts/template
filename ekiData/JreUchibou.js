2341e9a973e3da5b96263afc5f23fc9ecad0f112
miehorts <stroheimwt@yahoo.co.jp>
2023-08-27 17:19:55 +0900

    commit

---
+++ ekiData/JreUchibou.js
@@ -0,0 +1,84 @@
+const JreUchibouCommon = {
+    ...compNameJre,
+    lineName: '内房線',
+}
+
+const JreUchibou = [
+    {
+        ekimeiKanj: '千葉',
+        ekimeiKana: 'ちば',
+        ekimeiRoma: 'Chiba',
+        sttnAddr: '千葉市中央区新千葉一丁目1-1',
+        ...JreUchibouCommon,
+    },
+    {
+        ekimeiKanj: '本千葉',
+        ekimeiKana: 'ほんちば',
+        ekimeiRoma: 'Hon-Chiba',
+        sttnAddr: '千葉市中央区長洲一丁目30-1',
+        ...JreUchibouCommon,
+    },
+    {
+        ekimeiKanj: '蘇我',
+        ekimeiKana: 'そが',
+        ekimeiRoma: 'Soga',
+        sttnAddr: '千葉市中央区今井二丁目50-2',
+        ...JreUchibouCommon,
+    },
+    {
+        ekimeiKanj: '',
+        ekimeiKana: '',
+        ekimeiRoma: '',
+        sttnAddr: '',
+        ...JreUchibouCommon,
+    },
+    {
+        ekimeiKanj: '',
+        ekimeiKana: '',
+        ekimeiRoma: '',
+        sttnAddr: '',
+        ...JreUchibouCommon,
+    },
+    {
+        ekimeiKanj: '',
+        ekimeiKana: '',
+        ekimeiRoma: '',
+        sttnAddr: '',
+        ...JreUchibouCommon,
+    },
+    {
+        ekimeiKanj: '',
+        ekimeiKana: '',
+        ekimeiRoma: '',
+        sttnAddr: '',
+        ...JreUchibouCommon,
+    },
+    {
+        ekimeiKanj: '',
+        ekimeiKana: '',
+        ekimeiRoma: '',
+        sttnAddr: '',
+        ...JreUchibouCommon,
+    },
+    {
+        ekimeiKanj: '',
+        ekimeiKana: '',
+        ekimeiRoma: '',
+        sttnAddr: '',
+        ...JreUchibouCommon,
+    },
+    {
+        ekimeiKanj: '',
+        ekimeiKana: '',
+        ekimeiRoma: '',
+        sttnAddr: '',
+        ...JreUchibouCommon,
+    },
+    {
+        ekimeiKanj: '',
+        ekimeiKana: '',
+        ekimeiRoma: '',
+        sttnAddr: '',
+        ...JreUchibouCommon,
+    },
+];
