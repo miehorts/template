db52ccedeed406884db64b4788089a9d99f6de1e
miehorts <stroheimwt@yahoo.co.jp>
2023-09-07 17:49:12 +0900

    commit

---
+++ ekiData/jre/JreYahiko.js
@@ -0,0 +1,63 @@
+const JreYahikoCommon = {
+    ...compNameJre,
+    lineName: '弥彦線',
+}
+
+const JreYahiko = [
+    {
+        ekimeiKanj: '弥彦',
+        ekimeiKana: 'やひこ',
+        ekimeiRoma: 'Yahiko',
+        sttnAddr: '新潟県西蒲原郡弥彦村大字弥彦',
+        ...JreYahikoCommon
+    },
+    {
+        ekimeiKanj: '矢作',
+        ekimeiKana: 'やはぎ',
+        ekimeiRoma: 'Yahagi',
+        sttnAddr: '新潟県西蒲原郡弥彦村大字矢作',
+        ...JreYahikoCommon
+    },
+    {
+        ekimeiKanj: '吉田',
+        ekimeiKana: 'よしだ',
+        ekimeiRoma: 'Yoshida',
+        sttnAddr: '新潟県燕市吉田堤町1-1',
+        ...JreYahikoCommon
+    },
+    {
+        ekimeiKanj: '西燕',
+        ekimeiKana: 'にしつばめ',
+        ekimeiRoma: 'Nishi-Tsubame',
+        sttnAddr: '新潟県燕市花見富永分',
+        ...JreYahikoCommon
+    },
+    {
+        ekimeiKanj: '燕',
+        ekimeiKana: 'つばめ',
+        ekimeiRoma: 'Tsubame',
+        sttnAddr: '新潟県燕市燕（本町）2',
+        ...JreYahikoCommon
+    },
+    {
+        ekimeiKanj: '燕三条',
+        ekimeiKana: 'つばめさんじょう',
+        ekimeiRoma: 'Tsubamesanjō',
+        sttnAddr: '新潟県三条市下須頃497',
+        ...JreYahikoCommon
+    },
+    {
+        ekimeiKanj: '北三条',
+        ekimeiKana: 'きたさんじょう',
+        ekimeiRoma: 'Kita-Sanjō',
+        sttnAddr: '新潟県三条市元町',
+        ...JreYahikoCommon
+    },
+    {
+        ekimeiKanj: '東三条',
+        ekimeiKana: 'ひがしさんじょう',
+        ekimeiRoma: 'Higashi-Sanjō',
+        sttnAddr: '新潟県三条市東三条一丁目',
+        ...JreYahikoCommon
+    },
+];
--- main.js
+++ main.js
@@ -65,6 +65,7 @@ const lines = {
     JreIiyama,
     JreOoito,
     JreEchigo,
+    JreYahiko,
 };
 
 const ekiListAll = [];
