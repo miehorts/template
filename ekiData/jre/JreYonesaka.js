7fd6a856f422d511b727e081410d3f93f0f1a774
miehorts <stroheimwt@yahoo.co.jp>
2023-09-08 10:40:16 +0900

    commit

---
+++ ekiData/jre/JreYonesaka.js
@@ -0,0 +1,147 @@
+const JreYonesakaCommon = {
+    ...compNameJre,
+    lineName: '米坂線',
+}
+
+const JreYonesaka = [
+    {
+        ekimeiKanj: '米沢',
+        ekimeiKana: 'よねざわ',
+        ekimeiRoma: 'Yonezawa',
+        sttnAddr: '山形県米沢市駅前一丁目1-43',
+        ...JreYonesakaCommon
+    },
+    {
+        ekimeiKanj: '南米沢',
+        ekimeiKana: 'みなみよねざわ',
+        ekimeiRoma: 'Minami-Yonezawa',
+        sttnAddr: '山形県米沢市本町一丁目6',
+        ...JreYonesakaCommon
+    },
+    {
+        ekimeiKanj: '西米沢',
+        ekimeiKana: 'にしよねざわ',
+        ekimeiRoma: 'Nishi-Yonezawa',
+        sttnAddr: '山形県米沢市直江町4-92',
+        ...JreYonesakaCommon
+    },
+    {
+        ekimeiKanj: '成島',
+        ekimeiKana: 'なるしま',
+        ekimeiRoma: 'Narushima',
+        sttnAddr: '山形県米沢市大字成島ヤブサメ',
+        ...JreYonesakaCommon
+    },
+    {
+        ekimeiKanj: '中郡',
+        ekimeiKana: 'ちゅうぐん',
+        ekimeiRoma: 'Chūgun',
+        sttnAddr: '山形県東置賜郡川西町大字時田51',
+        ...JreYonesakaCommon
+    },
+    {
+        ekimeiKanj: '羽前小松',
+        ekimeiKana: 'うぜんこまつ',
+        ekimeiRoma: 'Uzen-Komatsu',
+        sttnAddr: '山形県東置賜郡川西町大字上小松1644',
+        ...JreYonesakaCommon
+    },
+    {
+        ekimeiKanj: '犬川',
+        ekimeiKana: 'いぬかわ',
+        ekimeiRoma: 'Inukawa',
+        sttnAddr: '山形県東置賜郡川西町大字小松',
+        ...JreYonesakaCommon
+    },
+    {
+        ekimeiKanj: '今泉',
+        ekimeiKana: 'いまいずみ',
+        ekimeiRoma: 'Imaizumi',
+        sttnAddr: '山形県長井市今泉',
+        ...JreYonesakaCommon
+    },
+    {
+        ekimeiKanj: '萩生',
+        ekimeiKana: 'はぎゅう',
+        ekimeiRoma: 'Hagyū',
+        sttnAddr: '山形県西置賜郡飯豊町大字萩生',
+        ...JreYonesakaCommon
+    },
+    {
+        ekimeiKanj: '羽前椿',
+        ekimeiKana: 'うぜんつばき',
+        ekimeiRoma: 'Uzen-Tsubaki',
+        sttnAddr: '山形県西置賜郡飯豊町大字椿1974-2',
+        ...JreYonesakaCommon
+    },
+    {
+        ekimeiKanj: '手ノ子',
+        ekimeiKana: 'てのこ',
+        ekimeiRoma: 'Tenoko',
+        sttnAddr: '山形県西置賜郡飯豊町大字手ノ子',
+        ...JreYonesakaCommon
+    },
+    {
+        ekimeiKanj: '羽前沼沢',
+        ekimeiKana: 'うぜんぬまざわ',
+        ekimeiRoma: 'Uzen-Numazawa',
+        sttnAddr: '山形県西置賜郡小国町大字沼沢',
+        ...JreYonesakaCommon
+    },
+    {
+        ekimeiKanj: '伊佐領',
+        ekimeiKana: 'いさりょう',
+        ekimeiRoma: 'Isaryō',
+        sttnAddr: '山形県西置賜郡小国町大字伊佐領',
+        ...JreYonesakaCommon
+    },
+    {
+        ekimeiKanj: '羽前松岡',
+        ekimeiKana: 'うぜんまつおか',
+        ekimeiRoma: 'Uzem-Matsuoka',
+        sttnAddr: '山形県西置賜郡小国町大字松岡',
+        ...JreYonesakaCommon
+    },
+    {
+        ekimeiKanj: '小国',
+        ekimeiKana: 'おぐに',
+        ekimeiRoma: 'Oguni',
+        sttnAddr: '山形県西置賜郡小国町大字岩井沢',
+        ...JreYonesakaCommon
+    },
+    {
+        ekimeiKanj: '越後金丸',
+        ekimeiKana: 'えちごかなまる',
+        ekimeiRoma: 'Echigo-Kanamaru',
+        sttnAddr: '新潟県岩船郡関川村大字金丸',
+        ...JreYonesakaCommon
+    },
+    {
+        ekimeiKanj: '越後片貝',
+        ekimeiKana: 'えちごかたかい',
+        ekimeiRoma: 'Echigo-Katakai',
+        sttnAddr: '新潟県岩船郡関川村大字片貝83',
+        ...JreYonesakaCommon
+    },
+    {
+        ekimeiKanj: '越後下関',
+        ekimeiKana: 'えちごしもせき',
+        ekimeiRoma: 'Echigo-Shimoseki',
+        sttnAddr: '新潟県岩船郡関川村大字下関',
+        ...JreYonesakaCommon
+    },
+    {
+        ekimeiKanj: '越後大島',
+        ekimeiKana: 'えちごおおしま',
+        ekimeiRoma: 'Echigo-Ōshima',
+        sttnAddr: '新潟県岩船郡関川村大字土沢',
+        ...JreYonesakaCommon
+    },
+    {
+        ekimeiKanj: '坂町',
+        ekimeiKana: 'さかまち',
+        ekimeiRoma: 'Sakamachi',
+        sttnAddr: '潟県村上市坂町',
+        ...JreYonesakaCommon
+    },
+];
--- main.js
+++ main.js
@@ -66,6 +66,7 @@ const lines = {
     JreOoito,
     JreEchigo,
     JreYahiko,
+    JreYonesaka,
 };
 
 const ekiListAll = [];
