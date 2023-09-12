a37a548e3c1f270811fe677dfd42a0b474343f5d
miehorts <stroheimwt@yahoo.co.jp>
2023-09-12 13:43:53 +0900

    commit

---
+++ ekiData/jre/JreYamada.js
@@ -0,0 +1,112 @@
+const JreYamadaCommon = {
+    ...compNameJre,
+    lineName: '山田線',
+}
+
+const JreYamada = [
+    {
+        ekimeiKanj: '盛岡',
+        ekimeiKana: 'もりおか',
+        ekimeiRoma: 'Morioka',
+        sttnAddr: '岩手県盛岡市盛岡駅前通1-48',
+        ...JreYamadaCommon
+    },
+    {
+        ekimeiKanj: '上盛岡',
+        ekimeiKana: 'かみもりおか',
+        ekimeiRoma: 'Kami-Morioka',
+        sttnAddr: '岩手県盛岡市本町通三丁目11',
+        ...JreYamadaCommon
+    },
+    {
+        ekimeiKanj: '山岸',
+        ekimeiKana: 'やまぎし',
+        ekimeiRoma: 'Yamagishi',
+        sttnAddr: '岩手県盛岡市山岸二丁目15',
+        ...JreYamadaCommon
+    },
+    {
+        ekimeiKanj: '上米内',
+        ekimeiKana: 'かみよない',
+        ekimeiRoma: 'Kamiyonai',
+        sttnAddr: '岩手県盛岡市上米内字中居20-2',
+        ...JreYamadaCommon
+    },
+    {
+        ekimeiKanj: '区界',
+        ekimeiKana: 'くざかい',
+        ekimeiRoma: 'Kuzakai',
+        sttnAddr: '岩手県宮古市区界1-1009',
+        ...JreYamadaCommon
+    },
+    {
+        ekimeiKanj: '松草',
+        ekimeiKana: 'まつくさ',
+        ekimeiRoma: 'Matsukusa',
+        sttnAddr: '岩手県宮古市区界4-28',
+        ...JreYamadaCommon
+    },
+    {
+        ekimeiKanj: '川内',
+        ekimeiKana: 'かわうち',
+        ekimeiRoma: 'Kawauchi',
+        sttnAddr: '岩手県宮古市川内6-48-3',
+        ...JreYamadaCommon
+    },
+    {
+        ekimeiKanj: '箱石',
+        ekimeiKana: 'はこいし',
+        ekimeiRoma: 'Hakoishi',
+        sttnAddr: '岩手県宮古市箱石',
+        ...JreYamadaCommon
+    },
+    {
+        ekimeiKanj: '陸中川井',
+        ekimeiKana: 'りくちゅうかわい',
+        ekimeiRoma: 'Rikuchū-Kawai',
+        sttnAddr: '岩手県宮古市川井5-102-10',
+        ...JreYamadaCommon
+    },
+    {
+        ekimeiKanj: '腹帯',
+        ekimeiKana: 'はらたい',
+        ekimeiRoma: 'Haratai',
+        sttnAddr: '	岩手県宮古市腹帯1-49',
+        ...JreYamadaCommon
+    },
+    {
+        ekimeiKanj: '茂市',
+        ekimeiKana: 'もいち',
+        ekimeiRoma: 'Moichi',
+        sttnAddr: '岩手県宮古市茂市3-159-9',
+        ...JreYamadaCommon
+    },
+    {
+        ekimeiKanj: '蟇目',
+        ekimeiKana: 'ひきめ',
+        ekimeiRoma: 'Hikime',
+        sttnAddr: '	岩手県宮古市蟇目12',
+        ...JreYamadaCommon
+    },
+    {
+        ekimeiKanj: '花原市',
+        ekimeiKana: 'けばらいち',
+        ekimeiRoma: 'Kebaraichi',
+        sttnAddr: '岩手県宮古市花原市',
+        ...JreYamadaCommon
+    },
+    {
+        ekimeiKanj: '千徳',
+        ekimeiKana: 'せんとく',
+        ekimeiRoma: 'Sentoku',
+        sttnAddr: '岩手県宮古市上鼻二丁目7-1',
+        ...JreYamadaCommon
+    },
+    {
+        ekimeiKanj: '宮古',
+        ekimeiKana: 'みやこ',
+        ekimeiRoma: 'MIYAKO',
+        sttnAddr: '岩手県宮古市宮町一丁目1-80',
+        ...JreYamadaCommon
+    },
+];
--- main.js
+++ main.js
@@ -76,6 +76,7 @@ const lines = {
     JreRikuusai,
     JreKitakami,
     JreKamaishi,
+    JreYamada,
 };
 
 const ekiListAll = [];
