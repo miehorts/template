a414467e49e2a63b7b978c283823c1510d9e8e28
miehorts <stroheimwt@yahoo.co.jp>
2023-09-29 16:35:01 +0900

    commit

---
+++ ekiData/keisei/KeiseiOshiage.js
@@ -0,0 +1,49 @@
+const KeiseiOshiageCommon = {
+    ...compNameKeisei,
+    lineName: '押上線',
+}
+
+const KeiseiOshiage = [
+    {
+        ekimeiKanj: '押上（スカイツリー前）',
+        ekimeiKana: 'おしあげ（すかいつりーまえ）',
+        ekimeiRoma: 'Oshiage (SKYTREE)',
+        sttnAddr: '東京都墨田区押上一丁目10-2',
+        ...KeiseiOshiageCommon
+    },
+    {
+        ekimeiKanj: '京成曳舟',
+        ekimeiKana: 'けいせいひきふね',
+        ekimeiRoma: 'Keisei-Hikifune',
+        sttnAddr: '東京都墨田区京島一丁目39番1号',
+        ...KeiseiOshiageCommon
+    },
+    {
+        ekimeiKanj: '八広',
+        ekimeiKana: 'やひろ',
+        ekimeiRoma: 'Yahiro',
+        sttnAddr: '東京都墨田区八広六丁目25番20号',
+        ...KeiseiOshiageCommon
+    },
+    {
+        ekimeiKanj: '四ツ木',
+        ekimeiKana: 'よつぎ',
+        ekimeiRoma: 'Yotsugi',
+        sttnAddr: '東京都葛飾区四つ木一丁目1番1号',
+        ...KeiseiOshiageCommon
+    },
+    {
+        ekimeiKanj: '京成立石',
+        ekimeiKana: 'けいせいたていし',
+        ekimeiRoma: 'Keisei-Tateishi',
+        sttnAddr: '東京都葛飾区立石四丁目24番1号',
+        ...KeiseiOshiageCommon
+    },
+    {
+        ekimeiKanj: '青砥',
+        ekimeiKana: 'あおと',
+        ekimeiRoma: 'Aoto',
+        sttnAddr: '東京都葛飾区青戸三丁目36番1号',
+        ...KeiseiOshiageCommon
+    },
+];
--- main.js
+++ main.js
@@ -102,6 +102,7 @@ const lines = {
     KeioInokashira,
     KeiseiHon,
     KeiseiHigashinarita,
+    KeiseiOshiage,
     TwrRinkai,
     TokyomonorailTokyomonorailhanedakuukou,
     HokuetsuexpressHokuhoku,
