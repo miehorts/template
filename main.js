b0b218e5d7dec66019f7aa6886871211c82118fe
miehorts <stroheimwt@yahoo.co.jp>
2023-10-11 16:15:52 +0900

    commit

--- main.js
+++ main.js
@@ -140,6 +140,7 @@ const lines = {
     TokyometroGinza,
     TokyometroMarunouchi,
     TokyometroTouzai,
+    TokyometroNanboku,
     TwrRinkai,
     TokyomonorailTokyomonorailhanedakuukou,
     HokuetsuexpressHokuhoku,
