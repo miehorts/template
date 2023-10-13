ba12e7a2f28b4c0c0c1aaf72499d3a46bda5e3e0
miehorts <stroheimwt@yahoo.co.jp>
2023-10-13 13:38:40 +0900

    commit

---
+++ ekiData/tobu/TobuSano.js
@@ -0,0 +1,77 @@
+const TobuSanoCommon = {
+    ...compNameTobu,
+    lineName: '佐野線',
+};
+
+const TobuSano = [
+    {
+        ekimeiKanj: '館林',
+        ekimeiKana: 'たてばやし',
+        ekimeiRoma: 'Tatebayashi',
+        sttnAddr: '群馬県館林市本町2-1-1',
+        ...TobuSanoCommon,
+    },
+    {
+        ekimeiKanj: '渡瀬',
+        ekimeiKana: 'わたらせ',
+        ekimeiRoma: 'Watarase',
+        sttnAddr: '群馬県館林市足次町65',
+        ...TobuSanoCommon,
+    },
+    {
+        ekimeiKanj: '田島',
+        ekimeiKana: 'たじま',
+        ekimeiRoma: 'Tajima',
+        sttnAddr: '栃木県佐野市田島町184',
+        ...TobuSanoCommon,
+    },
+    {
+        ekimeiKanj: '佐野市',
+        ekimeiKana: 'さのし',
+        ekimeiRoma: 'Sanoshi',
+        sttnAddr: '栃木県佐野市上台町2164',
+        ...TobuSanoCommon,
+    },
+    {
+        ekimeiKanj: '佐野',
+        ekimeiKana: 'さの',
+        ekimeiRoma: 'Sano',
+        sttnAddr: '栃木県佐野市若松町539',
+        ...TobuSanoCommon,
+    },
+    {
+        ekimeiKanj: '堀米',
+        ekimeiKana: 'ほりごめ',
+        ekimeiRoma: 'Horigome',
+        sttnAddr: '栃木県佐野市堀米町1274',
+        ...TobuSanoCommon,
+    },
+    {
+        ekimeiKanj: '吉水',
+        ekimeiKana: 'よしみず',
+        ekimeiRoma: 'Yoshimizu',
+        sttnAddr: '栃木県佐野市新吉水町60-1',
+        ...TobuSanoCommon,
+    },
+    {
+        ekimeiKanj: '田沼',
+        ekimeiKana: 'たぬま',
+        ekimeiRoma: 'Tanuma',
+        sttnAddr: '栃木県佐野市栃本町1766',
+        ...TobuSanoCommon,
+    },
+    {
+        ekimeiKanj: '多田',
+        ekimeiKana: 'ただ',
+        ekimeiRoma: 'Tada',
+        sttnAddr: '栃木県佐野市多田町1272-2',
+        ...TobuSanoCommon,
+    },
+    {
+        ekimeiKanj: '葛生',
+        ekimeiKana: 'くずう',
+        ekimeiRoma: 'Kuzu',
+        sttnAddr: '栃木県佐野市葛生東一丁目1-5',
+        ...TobuSanoCommon,
+    },
+];
--- main.js
+++ main.js
@@ -148,6 +148,7 @@ const lines = {
     TobuIsesaki,
     TobuKameido,
     TobuDaishi,
+    TobuSano,
     TwrRinkai,
     TokyomonorailTokyomonorailhanedakuukou,
     HokuetsuexpressHokuhoku,
