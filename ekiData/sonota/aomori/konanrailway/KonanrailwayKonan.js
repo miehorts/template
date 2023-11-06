164cd1f06ddcc7ed19121160be74471f64fb3f1b
miehorts <stroheimwt@yahoo.co.jp>
2023-11-06 19:30:13 +0900

    commit

---
+++ ekiData/sonota/aomori/konanrailway/KonanrailwayKonan.js
@@ -0,0 +1,120 @@
+const KonanrailwayKonanCommon = {
+    ...compNameKonanrailway,
+    lineName: '弘南線',
+    lineBack1: 'orange',
+}
+
+const KonanrailwayKonan = [
+    {
+        ekimeiKanj: '弘前',
+        ekimeiKana: 'ひろさき',
+        ekimeiRoma: 'Hirosaki',
+        sttnAddr: '青森県弘前市大字表町2-68',
+        ...KonanrailwayKonanCommon
+    },
+    {
+        ekimeiKanj: '弘前東高前',
+        ekimeiKana: 'ひろさきひがしこうまえ',
+        ekimeiRoma: 'HIROSAKI HIGASHI KO MAE',
+        sttnAddr: '青森県弘前市大字川先4丁目11-1',
+        ...KonanrailwayKonanCommon
+    },
+    {
+        ekimeiKanj: '運動公園前',
+        ekimeiKana: 'うんどうこうえんまえ',
+        ekimeiRoma: 'UNDO KOEN MAE',
+        sttnAddr: '青森県弘前市大字豊田2丁目4-91',
+        ...KonanrailwayKonanCommon
+    },
+    {
+        ekimeiKanj: '新里',
+        ekimeiKana: 'にさと',
+        ekimeiRoma: 'NISATO',
+        sttnAddr: '青森県弘前市大字新里字東里見14-2',
+        ...KonanrailwayKonanCommon
+    },
+    {
+        ekimeiKanj: '館田',
+        ekimeiKana: 'たちた',
+        ekimeiRoma: 'TACHITA',
+        sttnAddr: '青森県平川市館田中前田98-4',
+        ...KonanrailwayKonanCommon
+    },
+    {
+        ekimeiKanj: '平賀',
+        ekimeiKana: 'ひらか',
+        ekimeiRoma: 'HIRAKA',
+        sttnAddr: '青森県平川市本町北柳田23-5',
+        ...KonanrailwayKonanCommon
+    },
+    {
+        ekimeiKanj: '柏農高校前',
+        ekimeiKana: 'はくのうこうこうまえ',
+        ekimeiRoma: 'HAKUNO KOKO MAE',
+        sttnAddr: '青森県平川市荒田南岡部18-3',
+        ...KonanrailwayKonanCommon
+    },
+    {
+        ekimeiKanj: '津軽尾上',
+        ekimeiKana: 'つがるおのえ',
+        ekimeiRoma: 'TSUGARU ONOE',
+        sttnAddr: '青森県平川市中佐渡南田18-2',
+        ...KonanrailwayKonanCommon
+    },
+    {
+        ekimeiKanj: '尾上高校前',
+        ekimeiKana: 'おのえこうこうまえ',
+        ekimeiRoma: 'ONOE KOKO MAE',
+        sttnAddr: '青森県平川市猿賀上川50-3',
+        ...KonanrailwayKonanCommon
+    },
+    {
+        ekimeiKanj: '田んぼアート',
+        ekimeiKana: 'たんぼあーと',
+        ekimeiRoma: 'TAMBOĀTO',
+        sttnAddr: '青森県南津軽郡田舎館村',
+        ...KonanrailwayKonanCommon
+    },
+    {
+        ekimeiKanj: '',
+        ekimeiKana: '',
+        ekimeiRoma: '',
+        sttnAddr: '',
+        ...KonanrailwayKonanCommon
+    },
+    {
+        ekimeiKanj: '',
+        ekimeiKana: '',
+        ekimeiRoma: '',
+        sttnAddr: '',
+        ...KonanrailwayKonanCommon
+    },
+    {
+        ekimeiKanj: '',
+        ekimeiKana: '',
+        ekimeiRoma: '',
+        sttnAddr: '',
+        ...KonanrailwayKonanCommon
+    },
+    {
+        ekimeiKanj: '',
+        ekimeiKana: '',
+        ekimeiRoma: '',
+        sttnAddr: '',
+        ...KonanrailwayKonanCommon
+    },
+    {
+        ekimeiKanj: '',
+        ekimeiKana: '',
+        ekimeiRoma: '',
+        sttnAddr: '',
+        ...KonanrailwayKonanCommon
+    },
+    {
+        ekimeiKanj: '',
+        ekimeiKana: '',
+        ekimeiRoma: '',
+        sttnAddr: '',
+        ...KonanrailwayKonanCommon
+    },
+];
