1039a241d7e50cbedce0eaa4a50e5d43abe03aa9
miehorts <stroheimwt@yahoo.co.jp>
2023-11-14 19:25:21 +0900

    commit

---
+++ ekiData/jrc/JrcIida.js
@@ -0,0 +1,169 @@
+const JrcIidaCommon = {
+    ...compNameJrc,
+    lineName: '飯田線',
+    lineBack1: '#75a2db',
+};
+
+const JrcIida = [
+    {
+        ekimeiKanj: '豊橋',
+        ekimeiKana: 'とよはし',
+        ekimeiRoma: 'Toyohashi',
+        sttnAddr: '愛知県豊橋市花田町字西宿',
+        ...JrcIidaCommon
+    },
+    {
+        ekimeiKanj: '船町',
+        ekimeiKana: 'ふなまち',
+        ekimeiRoma: 'Funamachi',
+        sttnAddr: '愛知県豊橋市北島町字口戸',
+        ...JrcIidaCommon
+    },
+    {
+        ekimeiKanj: '下地',
+        ekimeiKana: 'しもじ',
+        ekimeiRoma: 'Shimoji',
+        sttnAddr: '愛知県豊橋市横須賀町後口',
+        ...JrcIidaCommon
+    },
+    {
+        ekimeiKanj: '小坂井',
+        ekimeiKana: 'こざかい',
+        ekimeiRoma: 'Kozakai',
+        sttnAddr: '愛知県豊川市小坂井町倉屋敷72',
+        ...JrcIidaCommon
+    },
+    {
+        ekimeiKanj: '牛久保',
+        ekimeiKana: 'うしくぼ',
+        ekimeiRoma: 'Ushikubo',
+        sttnAddr: '愛知県豊川市牛久保町城跡55',
+        ...JrcIidaCommon
+    },
+    {
+        ekimeiKanj: '豊川',
+        ekimeiKana: 'とよかわ',
+        ekimeiRoma: 'Toyokawa',
+        sttnAddr: '愛知県豊川市豊川町仁保通10',
+        ...JrcIidaCommon
+    },
+    {
+        ekimeiKanj: '三河一宮',
+        ekimeiKana: 'みかわいちのみや',
+        ekimeiRoma: 'Mikawa-ichinomiya',
+        sttnAddr: '愛知県豊川市一宮町下新切33',
+        ...JrcIidaCommon
+    },
+    {
+        ekimeiKanj: '長山',
+        ekimeiKana: 'ながやま',
+        ekimeiRoma: 'Nagayama',
+        sttnAddr: '愛知県豊川市上長山町西水神平',
+        ...JrcIidaCommon
+    },
+    {
+        ekimeiKanj: '江島',
+        ekimeiKana: 'えじま',
+        ekimeiRoma: 'Ejima',
+        sttnAddr: '愛知県豊川市東上町丸塚58',
+        ...JrcIidaCommon
+    },
+    {
+        ekimeiKanj: '東上',
+        ekimeiKana: 'とうじょう',
+        ekimeiRoma: 'Tōjō',
+        sttnAddr: '愛知県豊川市東上町東京寺46',
+        ...JrcIidaCommon
+    },
+    {
+        ekimeiKanj: '野田城',
+        ekimeiKana: 'のだじょう',
+        ekimeiRoma: 'Nodajō',
+        sttnAddr: '愛知県新城市野田字東浄悦2',
+        ...JrcIidaCommon
+    },
+    {
+        ekimeiKanj: '新城',
+        ekimeiKana: 'しんしろ',
+        ekimeiRoma: 'Shinshiro',
+        sttnAddr: '愛知県新城市宮ノ西30',
+        ...JrcIidaCommon
+    },
+    {
+        ekimeiKanj: '東新町',
+        ekimeiKana: 'ひがししんまち',
+        ekimeiRoma: 'Higashi-Shimmachi',
+        sttnAddr: '愛知県新城市平井字若杉12',
+        ...JrcIidaCommon
+    },
+    {
+        ekimeiKanj: '茶臼山',
+        ekimeiKana: 'ちゃうすやま',
+        ekimeiRoma: 'Chausuyama',
+        sttnAddr: '愛知県新城市富永字四条14',
+        ...JrcIidaCommon
+    },
+    {
+        ekimeiKanj: '三河東郷',
+        ekimeiKana: 'みかわとうごう',
+        ekimeiRoma: 'Mikawa-tōgō',
+        sttnAddr: '愛知県新城市川路字夜燈24',
+        ...JrcIidaCommon
+    },
+    {
+        ekimeiKanj: '大海',
+        ekimeiKana: 'おおみ',
+        ekimeiRoma: 'Ōmi',
+        sttnAddr: '愛知県新城市大海字南田49',
+        ...JrcIidaCommon
+    },
+    {
+        ekimeiKanj: '鳥居',
+        ekimeiKana: 'とりい',
+        ekimeiRoma: 'Torii',
+        sttnAddr: '愛知県新城市有海字島17',
+        ...JrcIidaCommon
+    },
+    {
+        ekimeiKanj: '長篠城',
+        ekimeiKana: 'ながしのじょう',
+        ekimeiRoma: 'Nagashinojō',
+        sttnAddr: '愛知県新城市長篠字森下19',
+        ...JrcIidaCommon
+    },
+    {
+        ekimeiKanj: '本長篠',
+        ekimeiKana: 'ほんながしの',
+        ekimeiRoma: 'Hon-Nagashino',
+        sttnAddr: '愛知県新城市長篠字具津67',
+        ...JrcIidaCommon
+    },
+    {
+        ekimeiKanj: '',
+        ekimeiKana: '',
+        ekimeiRoma: '',
+        sttnAddr: '',
+        ...JrcIidaCommon
+    },
+    {
+        ekimeiKanj: '',
+        ekimeiKana: '',
+        ekimeiRoma: '',
+        sttnAddr: '',
+        ...JrcIidaCommon
+    },
+    {
+        ekimeiKanj: '',
+        ekimeiKana: '',
+        ekimeiRoma: '',
+        sttnAddr: '',
+        ...JrcIidaCommon
+    },
+    {
+        ekimeiKanj: '',
+        ekimeiKana: '',
+        ekimeiRoma: '',
+        sttnAddr: '',
+        ...JrcIidaCommon
+    },
+];
