9dcb9b651012b90e2b3f1a60ecef238a0faa4fcd
miehorts <stroheimwt@yahoo.co.jp>
2023-12-06 19:26:32 +0900

    commit

---
+++ ekiData/meitetsu/MeitetsuKouwa.js
@@ -0,0 +1,120 @@
+const MeitetsuKouwaCommon = {
+    ...compNameMeitetsu,
+    lineName: '河和線',
+    lineBack1: '#2a9dd8',
+};
+
+const MeitetsuKouwa = [
+    {
+        ekimeiKanj: '太田川',
+        ekimeiKana: 'おおたがわ',
+        ekimeiRoma: 'ŌTAGAWA',
+        sttnAddr: '愛知県東海市大田町後田52',
+        ...MeitetsuKouwaCommon
+    },
+    {
+        ekimeiKanj: '高横須賀',
+        ekimeiKana: 'たかよこすか',
+        ekimeiRoma: 'TAKAYOKOSUKA',
+        sttnAddr: '愛知県東海市高横須賀町松本1-3',
+        ...MeitetsuKouwaCommon
+    },
+    {
+        ekimeiKanj: '南加木屋',
+        ekimeiKana: 'みなみかぎや',
+        ekimeiRoma: 'MINAMI KAGIYA',
+        sttnAddr: '愛知県東海市加木屋町南平井15',
+        ...MeitetsuKouwaCommon
+    },
+    {
+        ekimeiKanj: '八幡新田',
+        ekimeiKana: 'やわたしんでん',
+        ekimeiRoma: 'YAWATA-SHINDEN',
+        sttnAddr: '愛知県東海市加木屋町陀々法師43-1',
+        ...MeitetsuKouwaCommon
+    },
+    {
+        ekimeiKanj: '巽ヶ丘',
+        ekimeiKana: 'たつみがおか',
+        ekimeiRoma: 'TATSUMIGAOKA',
+        sttnAddr: '愛知県知多市巽が丘三丁目134',
+        ...MeitetsuKouwaCommon
+    },
+    {
+        ekimeiKanj: '白沢',
+        ekimeiKana: 'しらさわ',
+        ekimeiRoma: 'SHIRASAWA',
+        sttnAddr: '愛知県知多郡阿久比町白沢豊石山17-4',
+        ...MeitetsuKouwaCommon
+    },
+    {
+        ekimeiKanj: '坂部',
+        ekimeiKana: 'さかべ',
+        ekimeiRoma: 'SAKABE',
+        sttnAddr: '愛知県知多郡阿久比町卯坂上向田35-7',
+        ...MeitetsuKouwaCommon
+    },
+    {
+        ekimeiKanj: '阿久比',
+        ekimeiKana: 'あぐい',
+        ekimeiRoma: 'AGUI',
+        sttnAddr: '愛知県知多郡阿久比町阿久比駅前1丁目13',
+        ...MeitetsuKouwaCommon
+    },
+    {
+        ekimeiKanj: '植大',
+        ekimeiKana: 'うえだい',
+        ekimeiRoma: 'UEDAI',
+        sttnAddr: '愛知県知多郡阿久比町植大大前田4-3',
+        ...MeitetsuKouwaCommon
+    },
+    {
+        ekimeiKanj: '半田口',
+        ekimeiKana: 'はんだぐち',
+        ekimeiRoma: 'HANDAGUCHI',
+        sttnAddr: '愛知県半田市岩滑中町二丁目150',
+        ...MeitetsuKouwaCommon
+    },
+    {
+        ekimeiKanj: '住吉町',
+        ekimeiKana: 'すみよしちょう',
+        ekimeiRoma: 'SUMIYOSHICHŌ',
+        sttnAddr: '愛知県半田市宮路町156-2',
+        ...MeitetsuKouwaCommon
+    },
+    {
+        ekimeiKanj: '知多半田',
+        ekimeiKana: 'ちたはんだ',
+        ekimeiRoma: 'CHITA HANDA',
+        sttnAddr: '愛知県半田市広小路町128-1',
+        ...MeitetsuKouwaCommon
+    },
+    {
+        ekimeiKanj: '成岩',
+        ekimeiKana: 'ならわ',
+        ekimeiRoma: 'NARAWA',
+        sttnAddr: '愛知県半田市栄町三丁目304',
+        ...MeitetsuKouwaCommon
+    },
+    {
+        ekimeiKanj: '',
+        ekimeiKana: '',
+        ekimeiRoma: '',
+        sttnAddr: '',
+        ...MeitetsuKouwaCommon
+    },
+    {
+        ekimeiKanj: '',
+        ekimeiKana: '',
+        ekimeiRoma: '',
+        sttnAddr: '',
+        ...MeitetsuKouwaCommon
+    },
+    {
+        ekimeiKanj: '',
+        ekimeiKana: '',
+        ekimeiRoma: '',
+        sttnAddr: '',
+        ...MeitetsuKouwaCommon
+    },
+];
