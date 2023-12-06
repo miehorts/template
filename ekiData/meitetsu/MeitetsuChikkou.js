9211519c9076aa68734afdcaf44b944b62a0f681
miehorts <stroheimwt@yahoo.co.jp>
2023-12-06 15:12:11 +0900

    commit

---
+++ ekiData/meitetsu/MeitetsuChikkou.js
@@ -0,0 +1,22 @@
+const MeitetsuChikkouCommon = {
+    ...compNameMeitetsu,
+    lineName: '築港線',
+    lineBack1: '#727171',
+};
+
+const MeitetsuChikkou = [
+    {
+        ekimeiKanj: '大江',
+        ekimeiKana: 'おおえ',
+        ekimeiRoma: 'ŌE',
+        sttnAddr: '名古屋市南区加福本通二丁目8-1',
+        ...MeitetsuChikkouCommon
+    },
+    {
+        ekimeiKanj: '東名古屋港',
+        ekimeiKana: 'ひがしなごやこう',
+        ekimeiRoma: 'HIGASHI NAGOYAKŌ',
+        sttnAddr: '名古屋市港区大江町1-6',
+        ...MeitetsuChikkouCommon
+    },
+];
--- main.js
+++ main.js
@@ -187,6 +187,7 @@ const lines = {
     MeitetsuToyota,
     MeitetsuTokoname,
     MeitetsuKuukou,
+    MeitetsuChikkou,
     //準大手私鉄
     //関東地方
     ShinkeiseiShinkeisei,
