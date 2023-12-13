1ba5ddc7461ce7b504404212a1d32fb5bb8a5b64
miehorts <stroheimwt@yahoo.co.jp>
2023-12-13 13:36:34 +0900

    commit

---
+++ ekiData/meitetsu/MeitetsuHashima.js
@@ -0,0 +1,22 @@
+const MeitetsuHashimaCommon = {
+    ...compNameMeitetsu,
+    lineName: '羽島線',
+    lineBack1: '#d7a629',
+};
+
+const MeitetsuHashima = [
+    {
+        ekimeiKanj: '江吉良',
+        ekimeiKana: 'えぎら',
+        ekimeiRoma: 'EGIRA',
+        sttnAddr: '	岐阜県羽島市江吉良町江東839',
+        ...MeitetsuHashimaCommon
+    },
+    {
+        ekimeiKanj: '新羽島',
+        ekimeiKana: 'しんはしま',
+        ekimeiRoma: 'SHIN HASHIMA',
+        sttnAddr: '	岐阜県羽島市舟橋町宮北一丁目1番地1',
+        ...MeitetsuHashimaCommon
+    },
+];
