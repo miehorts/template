49f115df7f27f0987e194e678a57ef2fed883128
miehorts <stroheimwt@yahoo.co.jp>
2024-01-03 16:16:27 +0900

    commit

--- ekiData/sonota/tochigi/utsunomiyalightrail/UtsunomiyalightrailUtsunomiyahagalightrail.js
+++ ekiData/sonota/tochigi/utsunomiyalightrail/UtsunomiyalightrailUtsunomiyahagalightrail.js
@@ -15,132 +15,132 @@ const UtsunomiyalightrailUtsunomiyahagalightrail = [
         ekimeiKana: 'うつのみやえきひがしぐち',
         ekimeiRoma: 'Utsunomiya Station East',
         sttnAddr: '栃木県宇都宮市宮みらい一丁目',
-        ...UtsunomiyalightrailUtsunomiyahagalightrailCommon
+        ...UtsunomiyalightrailUtsunomiyahagalightrailCommon1
     },
     {
         ekimeiKanj: '東宿郷',
         ekimeiKana: 'ひがししゅくごう',
         ekimeiRoma: 'Higashi-Shukugō',
         sttnAddr: '栃木県宇都宮市東宿郷',
-        ...UtsunomiyalightrailUtsunomiyahagalightrailCommon
+        ...UtsunomiyalightrailUtsunomiyahagalightrailCommon1
     },
     {
         ekimeiKanj: '駅東公園前',
         ekimeiKana: 'えきひがしこうえんまえ',
         ekimeiRoma: 'Ekihigashi Park',
         sttnAddr: '栃木県宇都宮市東宿郷',
-        ...UtsunomiyalightrailUtsunomiyahagalightrailCommon
+        ...UtsunomiyalightrailUtsunomiyahagalightrailCommon1
     },
     {
         ekimeiKanj: '峰',
         ekimeiKana: 'みね',
         ekimeiRoma: 'Mine',
         sttnAddr: '栃木県宇都宮市峰',
-        ...UtsunomiyalightrailUtsunomiyahagalightrailCommon
+        ...UtsunomiyalightrailUtsunomiyahagalightrailCommon1
     },
     {
         ekimeiKanj: '陽東3丁目',
         ekimeiKana: 'ようとうさんちょうめ',
         ekimeiRoma: 'Yōtō 3-chōme',
         sttnAddr: '栃木県宇都宮市陽東',
-        ...UtsunomiyalightrailUtsunomiyahagalightrailCommon
+        ...UtsunomiyalightrailUtsunomiyahagalightrailCommon1
     },
     {
         ekimeiKanj: '宇都宮大学陽東キャンパス',
         ekimeiKana: 'うつのみやだいがくようとうキャンパス',
         ekimeiRoma: 'Utsunomiya Univ. Yōtō Campus',
         sttnAddr: '栃木県宇都宮市陽東',
-        ...UtsunomiyalightrailUtsunomiyahagalightrailCommon
+        ...UtsunomiyalightrailUtsunomiyahagalightrailCommon1
     },
     {
         ekimeiKanj: '平石',
         ekimeiKana: 'ひらいし',
         ekimeiRoma: 'Hiraishi',
         sttnAddr: '栃木県宇都宮市下平出町3110番地',
-        ...UtsunomiyalightrailUtsunomiyahagalightrailCommon
+        ...UtsunomiyalightrailUtsunomiyahagalightrailCommon1
     },
     {
         ekimeiKanj: '平石中央小学校前',
         ekimeiKana: 'ひらいしちゅうおうしょうがっこうまえ',
         ekimeiRoma: 'Hiraishi-chūō Elem. Sch.',
         sttnAddr: '栃木県宇都宮市下平出町',
-        ...UtsunomiyalightrailUtsunomiyahagalightrailCommon
+        ...UtsunomiyalightrailUtsunomiyahagalightrailCommon1
     },
     {
         ekimeiKanj: '飛山城跡',
         ekimeiKana: 'とびやまじょうあと',
         ekimeiRoma: 'Tobiyama Castle Site',
         sttnAddr: '栃木県宇都宮市竹下町',
-        ...UtsunomiyalightrailUtsunomiyahagalightrailCommon
+        ...UtsunomiyalightrailUtsunomiyahagalightrailCommon1
     },
     {
         ekimeiKanj: '清陵高校前',
         ekimeiKana: 'せいりょうこうこうまえ',
         ekimeiRoma: 'Seiryō High Sch.',
         sttnAddr: '栃木県宇都宮市竹下町',
-        ...UtsunomiyalightrailUtsunomiyahagalightrailCommon
+        ...UtsunomiyalightrailUtsunomiyahagalightrailCommon1
     },
     {
         ekimeiKanj: '清原地区市民センター前',
         ekimeiKana: 'きよはらちくしみんセンターまえ',
         ekimeiRoma: 'Kiyohara District Civic Center',
         sttnAddr: '栃木県宇都宮市清原工業団地',
-        ...UtsunomiyalightrailUtsunomiyahagalightrailCommon
+        ...UtsunomiyalightrailUtsunomiyahagalightrailCommon1
     },
     {
         ekimeiKanj: 'グリーンスタジアム前',
         ekimeiKana: 'グリーンスタジアムまえ',
         ekimeiRoma: 'Green Stadium',
         sttnAddr: '栃木県宇都宮市清原工業団地',
-        ...UtsunomiyalightrailUtsunomiyahagalightrailCommon
+        ...UtsunomiyalightrailUtsunomiyahagalightrailCommon1
     },
     {
         ekimeiKanj: 'ゆいの杜西',
         ekimeiKana: 'ゆいのもりにし',
         ekimeiRoma: 'Yuinomori-west',
         sttnAddr: '栃木県宇都宮市ゆいの杜1丁目',
-        ...UtsunomiyalightrailUtsunomiyahagalightrailCommon
+        ...UtsunomiyalightrailUtsunomiyahagalightrailCommon1
     },
     {
         ekimeiKanj: 'ゆいの杜中央',
         ekimeiKana: 'ゆいのもりちゅうおう',
         ekimeiRoma: 'Yuinomori-central',
         sttnAddr: '栃木県宇都宮市ゆいの杜4丁目',
-        ...UtsunomiyalightrailUtsunomiyahagalightrailCommon
+        ...UtsunomiyalightrailUtsunomiyahagalightrailCommon1
     },
     {
         ekimeiKanj: 'ゆいの杜東',
         ekimeiKana: 'ゆいのもりひがし',
         ekimeiRoma: 'Yuinomori-east',
         sttnAddr: '栃木県宇都宮市ゆいの杜5丁目',
-        ...UtsunomiyalightrailUtsunomiyahagalightrailCommon
+        ...UtsunomiyalightrailUtsunomiyahagalightrailCommon1
     },
     {
         ekimeiKanj: '芳賀台',
         ekimeiKana: 'はがだい',
         ekimeiRoma: 'Hagadai',
         sttnAddr: '栃木県芳賀郡芳賀町芳賀台',
-        ...UtsunomiyalightrailUtsunomiyahagalightrailCommon
+        ...UtsunomiyalightrailUtsunomiyahagalightrailCommon1
     },
     {
         ekimeiKanj: '芳賀町工業団地管理センター前',
         ekimeiKana: 'はがまちこうぎょうだんちかんりセンターまえ',
         ekimeiRoma: 'Haga Industrial Park Management Center',
         sttnAddr: '栃木県芳賀郡芳賀町芳賀台',
-        ...UtsunomiyalightrailUtsunomiyahagalightrailCommon
+        ...UtsunomiyalightrailUtsunomiyahagalightrailCommon1
     },
     {
         ekimeiKanj: 'かしの森公園前',
         ekimeiKana: 'かしのもりこうえんまえ',
         ekimeiRoma: 'Kashinomori Park',
         sttnAddr: '栃木県芳賀郡芳賀町下高根沢',
-        ...UtsunomiyalightrailUtsunomiyahagalightrailCommon
+        ...UtsunomiyalightrailUtsunomiyahagalightrailCommon1
     },
     {
         ekimeiKanj: '芳賀・高根沢工業団地',
         ekimeiKana: 'はが・たかねざわこうぎょうだんち',
         ekimeiRoma: 'Haga Takanezawa Industrial Park',
         sttnAddr: '栃木県芳賀郡芳賀町大字下高根沢',
-        ...UtsunomiyalightrailUtsunomiyahagalightrailCommon
+        ...UtsunomiyalightrailUtsunomiyahagalightrailCommon1
     },
 ];
